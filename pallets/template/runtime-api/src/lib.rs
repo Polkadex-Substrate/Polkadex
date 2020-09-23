6ee8431485427dcd262448ef0f262db561c4c4aa
Gautham J <Gauthamastro@gmail.com>
2020-09-23 19:45:56 +0530

    Fixed it!

--- pallets/template/runtime-api/src/lib.rs
+++ pallets/template/runtime-api/src/lib.rs
@@ -2,11 +2,11 @@
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::unnecessary_mut_passed)]
 
-use sp_arithmetic::{FixedPointNumber, FixedU128};
-use sp_runtime::traits::Hash;
+use sp_arithmetic::FixedU128;
+use sp_core::H256;
 
 sp_api::decl_runtime_apis! {
 	pub trait DexStorageApi {
-		fn get_ask_level(trading_pair: Hash) -> Vec<FixedU128>;
+		fn get_ask_level(trading_pair: H256) -> Vec<FixedU128>;
 	}
 }
