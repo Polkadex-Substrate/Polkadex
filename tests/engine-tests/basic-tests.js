e3e3bb05412c8578ca407fc79d4cff25113ff6cc
Gautham J <Gauthamastro@gmail.com>
2020-10-02 15:18:58 +0530

    Added Tests for Limit Orders

--- tests/engine-tests/basic-tests.js
+++ tests/engine-tests/basic-tests.js
@@ -5,7 +5,7 @@ const {cryptoWaitReady} = require('@polkadot/util-crypto');
 
 
 // Construct
-const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+const wsProvider = new WsProvider('ws://0.0.0.0:9944');
 temp().then(r => console.log("Exited"))
 
 async function temp() {
@@ -57,7 +57,7 @@ async function temp() {
             "Address": "AccountId"
         },
     });
-
+    const tradingPairID = "0xf28a3c76161b8d5723b6b8b092695f418037c747faa2ad8bc33d8871f720aac9";
     const UNIT = 1000000000000;
     let options = {
         permissions: {
@@ -67,39 +67,65 @@ async function temp() {
         }
     }
     // Create first token - Say USDT
-    await api.tx.genericAsset.create([1000 * UNIT, options]).signAndSend(alice, {nonce: 0}, (result) => {
-        console.log(`Alice's Current status is ${result.status}`);
-        if (result.status.isInBlock) {
-            console.log(`Create-Token #1 Transaction included at blockHash ${result.status.asInBlock}`);
-        } else if (result.status.isFinalized) {
-            console.log(`Create-Token #1 Transaction finalized at blockHash ${result.status.asFinalized}`);
-        }
-    });
+    await api.tx.genericAsset.create([1000 * UNIT, options]).signAndSend(alice, {nonce: 0});
     // Create second token - Say BTC
-    await api.tx.genericAsset.create([1000 * UNIT, options]).signAndSend(bob, {nonce: 0}, (result) => {
-        console.log(`Bob's Current status is ${result.status}`);
-        if (result.status.isInBlock) {
-            console.log(`Create-Token #2 Transaction included at blockHash ${result.status.asInBlock}`);
-        } else if (result.status.isFinalized) {
-            console.log(`Create-Token #2 Transaction finalized at blockHash ${result.status.asFinalized}`);
-        }
-    });
-
+    await api.tx.genericAsset.create([UNIT, options]).signAndSend(bob, {nonce: 0});
     // Note token created first has Token ID as 1 and second token has ID 2.
     // Create the tradingPair BTC/USDT - (2,1)
-    await api.tx.templateModule.registerNewOrderbook(2, 1).signAndSend(alice, { nonce: 1 },(result) => {
-        if (result.status.isInBlock) {
-            console.log(`RegisterTradingPair Transaction included at blockHash ${result.status.asInBlock}`);
-            result.events.forEach(({event: {data, method, section}, phase}) => {
-                console.log('Events: ', phase.toString(), `: ${section}.${method}`, data.toString());
-            });
-        } else if (result.status.isFinalized) {
-            console.log(`RegisterTradingPair Transaction finalized at blockHash ${result.status.asFinalized}`);
-        }
-    });
+    await api.tx.polkadex.registerNewOrderbook(2, 1).signAndSend(alice, { nonce: 1 });
 
     // Let's place some orders and check if we got the expected results
-    // Alice places a buy order at unit price 1 for 1 unit quantity
-    // await api.tx.templateModule.submitOrder("BidLimit", tradingPairID, UNIT, UNIT).signAndSend(alice,{ nonce: 2 });
+    // Alice places buy limit orders
+    await api.tx.polkadex.submitOrder("BidLimit", tradingPairID, 820*UNIT, 0.2*UNIT).signAndSend(alice,{ nonce: 2 });
+    await api.tx.polkadex.submitOrder("BidLimit", tradingPairID, 800*UNIT, 0.1*UNIT).signAndSend(alice,{ nonce: 3 });
+    await api.tx.polkadex.submitOrder("BidLimit", tradingPairID, 850*UNIT, 0.1*UNIT).signAndSend(alice,{ nonce: 4 });
+    await api.tx.polkadex.submitOrder("BidLimit", tradingPairID, 840*UNIT, 0.1*UNIT).signAndSend(alice,{ nonce: 5 });
+    await api.tx.polkadex.submitOrder("BidLimit", tradingPairID, 900*UNIT, 0.1*UNIT).signAndSend(alice,{ nonce: 6 });
+    // Bob places sell limit orders
+    await api.tx.polkadex.submitOrder("AskLimit", tradingPairID, 1075*UNIT, 0.2*UNIT).signAndSend(bob,{ nonce: 1 });
+    await api.tx.polkadex.submitOrder("AskLimit", tradingPairID, 1100*UNIT, 0.1*UNIT).signAndSend(bob,{ nonce: 2 });
+    await api.tx.polkadex.submitOrder("AskLimit", tradingPairID, 1060*UNIT, 0.1*UNIT).signAndSend(bob,{ nonce: 3 });
+    await api.tx.polkadex.submitOrder("AskLimit", tradingPairID, 1040*UNIT, 0.1*UNIT).signAndSend(bob,{ nonce: 4 });
+    await api.tx.polkadex.submitOrder("AskLimit", tradingPairID, 1000*UNIT, 0.1*UNIT).signAndSend(bob,{ nonce: 5 });
+
+    // Query the storage about the values
+    // Test #1
+    await api.query.genericAsset.freeBalance(1,alice.address, (balance)=>{
+        console.log("Free balance of Alice: ",balance.toNumber());
+        if (balance.toNumber() === 497*UNIT){
+           console.log("ALICE-FREE-BALANCE-TEST: Passed")
+        }else{
+            console.log("ALICE-FREE-BALANCE-TEST: Failed")
+        }
+    })
+    // Test #2
+    await api.query.genericAsset.freeBalance(2,bob.address, (balance)=>{
+        console.log("Free balance of Alice: ",balance.toNumber());
+        if (balance.toNumber() === 0.4*UNIT){
+            console.log("BOB-FREE-BALANCE-TEST: Passed")
+        }else{
+            console.log("BOB-FREE-BALANCE-TEST: Failed")
+        }
+    })
+    // Test #3
+    await api.query.genericAsset.reservedBalance(1,alice.address, (balance)=>{
+        console.log("Reserved balance of Alice: ",balance.toNumber());
+        if (balance.toNumber() === 503*UNIT){
+            console.log("ALICE-RESERVED-BALANCE-TEST: Passed")
+        }else{
+            console.log("ALICE-RESERVED-BALANCE-TEST: Failed")
+        }
+    })
+    // Test #4
+    await api.query.genericAsset.reservedBalance(2,bob.address, (balance)=>{
+        console.log("Reserved balance of Alice: ",balance.toNumber());
+        if (balance.toNumber() === 0.6*UNIT){
+            console.log("BOB-RESERVED-BALANCE-TEST: Passed")
+        }else{
+            console.log("BOB-RESERVED-BALANCE-TEST: Failed")
+        }
+    })
+
+    // TODO Add Market Orders
 
 }
