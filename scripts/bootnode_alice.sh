8a03b68bcb926971912bde45773bd83c1290293c
Gautham J <Gauthamastro@gmail.com>
2020-10-14 23:13:12 +0530

    Added Scripts to boot testing nodes

---
+++ scripts/bootnode_alice.sh
@@ -0,0 +1,9 @@
+../target/release/node-polkadex \
+  --base-path /tmp/alice \
+  --chain customSpecRaw.json \
+  --alice \
+  --port 30333 \
+  --ws-port 9944 \
+  --rpc-port 9933 \
+  --node-key 0000000000000000000000000000000000000000000000000000000000000001 \
+  --validator
---
+++ scripts/validator_dave.sh
@@ -0,0 +1,10 @@
+../target/release/node-polkadex \
+  --base-path /tmp/bob \
+  --chain customSpecRaw.json \
+  --dave \
+  --port 30334 \
+  --ws-port 9945 \
+  --rpc-port 9934 \
+  --validator \
+  --node-key 0000000000000000000000000000000000000000000000000000000000000004 \
+  --bootnodes /ip4/13.235.190.203/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
