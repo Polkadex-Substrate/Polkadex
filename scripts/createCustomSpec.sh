84d8b9889d1a8166886dfc6666efe335e2d39915
Ubuntu <ubuntu@ip-172-31-34-114.us-east-2.compute.internal>
2021-01-21 11:36:29 +0000

    Added json script

--- scripts/createCustomSpec.sh
+++ scripts/createCustomSpec.sh
@@ -1,2 +1,2 @@
-../target/release/node-polkadex build-spec --disable-default-bootnode --chain local > customSpec.json
-../target/release/node-polkadex build-spec --chain=customSpec.json --raw --disable-default-bootnode > customSpecRaw.json
+../target/release/substrate build-spec --disable-default-bootnode --chain local > customSpec.json
+../target/release/substrate build-spec --chain=customSpec.json --raw --disable-default-bootnode > customSpecRaw.json
--- scripts/validator_bob.sh
+++ scripts/validator_bob.sh
@@ -7,4 +7,4 @@
   --rpc-port 9934 \
   --validator \
   --node-key 0000000000000000000000000000000000000000000000000000000000000002 \
-  --bootnodes /ip4/13.235.190.203/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
+  --bootnodes /ip4/18.217.102.97/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
