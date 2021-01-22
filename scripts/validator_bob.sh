d644ecd4cff25363a6a86dccf552e41820407928
ksr30 <krishna.singh9926@gmail.com>
2021-01-22 05:23:14 +0000

    added new changes

--- scripts/validator_bob.sh
+++ scripts/validator_bob.sh
@@ -1,4 +1,5 @@
-../target/release/node-polkadex \
+../target/release/substrate purge-chain --base-path /tmp/bob --chain local
+../target/release/substrate \
   --base-path /tmp/bob \
   --chain customSpecRaw.json \
   --bob \
@@ -7,4 +8,4 @@
   --rpc-port 9934 \
   --validator \
   --node-key 0000000000000000000000000000000000000000000000000000000000000002 \
-  --bootnodes /ip4/18.217.102.97/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
+  --bootnodes /ip4/3.18.111.13/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
