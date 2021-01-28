db46e915563cd4cff5728c9d3243adeb623be269
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2021-01-28 14:26:05 +0530

    #80 Modify add_registrat banchmarking

--- pallets/polkadex-identity-pallet/src/benchmarking.rs
+++ pallets/polkadex-identity-pallet/src/benchmarking.rs
@@ -2,17 +2,29 @@
 
 use super::*;
 use sp_std::prelude::*;
-use frame_system::RawOrigin;
+use frame_system::{EventRecord, RawOrigin};
 use frame_support::{ensure, traits::OnFinalize,};
 use frame_benchmarking::{benchmarks, TrackedStorageKey, account};
 
 const SEED: u32 = 0;
 
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+    let events = frame_system::Module::<T>::events();
+    let system_event: <T as frame_system::Config>::Event = generic_event.into();
+    // compare to the last event record
+    let EventRecord { event, .. } = &events[events.len() - 1];
+    assert_eq!(event, &system_event);
+}
+
 benchmarks! {
 
 	add_registrar {
-		let account_id = account("registrar", 0, SEED);
-	}: _(RawOrigin::Root, account_id)
+		let account: T::AccountId = account("registrar", 0, SEED);
+	}: _(RawOrigin::Root, account.clone())
+	verify {
+	    ensure!(Registrars::<T>::get(&account) == Judgement::PolkadexFoundationAccount, "RegistrarAlreadyPresent");
+		assert_last_event::<T>(Event::<T>::RegistrarAdded(account).into());
+	}
 
 }
 
