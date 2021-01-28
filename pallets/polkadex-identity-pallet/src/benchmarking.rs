c02acf2086fc5e51b2ea319c7c1f726410c693d9
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2021-01-28 21:33:52 +0530

    #80 Added/modified benchmarking of idenity pallet

--- pallets/polkadex-identity-pallet/src/benchmarking.rs
+++ pallets/polkadex-identity-pallet/src/benchmarking.rs
@@ -1,12 +1,21 @@
 #![cfg(feature = "runtime-benchmarks")]
 
 use super::*;
-use sp_std::prelude::*;
 use frame_system::{EventRecord, RawOrigin};
-use frame_support::{ensure, traits::OnFinalize,};
-use frame_benchmarking::{benchmarks, TrackedStorageKey, account};
-
+use frame_support::ensure;
+use frame_benchmarking::{benchmarks, account, whitelisted_caller};
+use crate::Module as Identity;
 const SEED: u32 = 0;
+
+fn add_sub_accounts<T: Config>(who: &T::AccountId, s: u32) -> Result<Vec<T::AccountId>, &'static str> {
+    let mut subs: Vec<T::AccountId> = Vec::new();
+    for i in 0..s {
+        let sub_account: T::AccountId = account("sub", i, SEED);
+        subs.push(sub_account.clone());
+    }
+    <SubsOf<T>>::insert(who, subs.clone());
+    Ok(subs)
+}
 
 fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
     let events = frame_system::Module::<T>::events();
@@ -22,8 +31,31 @@ benchmarks! {
 		let account: T::AccountId = account("registrar", 0, SEED);
 	}: _(RawOrigin::Root, account.clone())
 	verify {
-	    ensure!(Registrars::<T>::get(&account) == Judgement::PolkadexFoundationAccount, "RegistrarAlreadyPresent");
+	    ensure!(Registrars::<T>::contains_key(&account), "Registrars not added.");
 		assert_last_event::<T>(Event::<T>::RegistrarAdded(account).into());
+	}
+
+	provide_judgement_trader {
+	    let caller: T::AccountId = whitelisted_caller();
+        let account: T::AccountId = account("new", 0, SEED);
+        Identity::<T>::add_registrar(RawOrigin::Root.into(), caller.clone())?;
+	}: _(RawOrigin::Signed(caller), account.clone(), Judgement::Reasonable)
+	verify {
+		assert_last_event::<T>(Event::<T>::JudgementGiven(account).into());
+	}
+
+	add_sub_account {
+        let s in 1 .. T::MaxSubAccounts::get() - 1;
+
+		let caller: T::AccountId = whitelisted_caller();
+		let _ = add_sub_accounts::<T>(&caller, s)?;
+		let sub = account("new_sub", 0, SEED);
+        <IdentityOf<T>>::insert(&caller, Judgement::Reasonable);
+		ensure!(SubsOf::<T>::get(&caller).len() as u32 == s, "Subs not set.");
+	}: _(RawOrigin::Signed(caller.clone()), sub)
+	verify {
+		ensure!(SubsOf::<T>::get(&caller).len() as u32 == s + 1, "Subs not added.");
+		assert_last_event::<T>(Event::<T>::SubIdentityAdded.into());
 	}
 
 }
@@ -38,6 +70,8 @@ mod tests {
     fn test_benchmarks() {
         new_test_ext().execute_with(|| {
             assert_ok!(test_benchmark_add_registrar::<Test>());
+            assert_ok!(test_benchmark_provide_judgement_trader::<Test>());
+            assert_ok!(test_benchmark_add_sub_account::<Test>());
         });
     }
 }
