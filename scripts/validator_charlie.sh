a5904257e87c6ba6ed539ce4421099db19f7a0c8
ksr30 <krishna.singh9926@gmail.com>
2021-02-02 21:53:49 +0530

    changed ip

--- scripts/validator_charlie.sh
+++ scripts/validator_charlie.sh
@@ -7,4 +7,4 @@
   --rpc-port 9934 \
   --validator \
   --node-key 0000000000000000000000000000000000000000000000000000000000000003 \
-  --bootnodes /ip4/13.235.190.203/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
+  --bootnodes /ip4/18.191.229.114/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
