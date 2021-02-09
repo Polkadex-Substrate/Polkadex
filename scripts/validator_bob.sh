1da10cecfceacba858f974de17d2b20b62aca459
ksr30 <krishna.singh9926@gmail.com>
2021-02-09 05:49:13 +0000

    updated script

--- scripts/validator_bob.sh
+++ scripts/validator_bob.sh
@@ -8,4 +8,4 @@
   --rpc-port 9934 \
   --validator \
   --node-key 0000000000000000000000000000000000000000000000000000000000000002 \
-  --bootnodes /ip4/3.15.171.128/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
+  --bootnodes /ip4/3.133.83.111/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
