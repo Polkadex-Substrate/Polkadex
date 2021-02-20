13cc5139d30d0439619696e22ca132c30ac8abf9
Gautham J <Gauthamastro@gmail.com>
2021-02-20 20:35:06 +0530

    Upgrade Develop to Aura

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,10 +1,12 @@
-#![warn(unused_extern_crates)]
+//! Substrate Node Template CLI library.
+#![warn(missing_docs)]
 
 mod chain_spec;
 #[macro_use]
 mod service;
 mod cli;
 mod command;
+mod rpc;
 
 fn main() -> sc_cli::Result<()> {
 	command::run()
