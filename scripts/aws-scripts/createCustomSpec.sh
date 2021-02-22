584fc8ea17c800e812d4b64820ae16d826d9ebdf
Pranayagaram <50867481+Pranayagaram@users.noreply.github.com>
2021-02-22 14:44:20 +0530

    changed the folder formate file 1.5

--- scripts/aws-scripts/createCustomSpec.sh
+++ scripts/aws-scripts/createCustomSpec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/polkadex-aura-node/target/release/node-template build-spec  --chain local > /polkadex-aura-node/scripts/customSpec.json
-/polkadex-aura-node/target/release/node-template build-spec --chain=customSpec.json --raw > /polkadex-aura-node/scripts/customSpecRaw.json
+/polkadex/target/release/node-template build-spec  --chain local > /polkadex/scripts/customSpec.json
+/polkadex/target/release/node-template build-spec --chain=customSpec.json --raw > /polkadex/scripts/customSpecRaw.json
--- scripts/aws-scripts/validator_charlie-aws.sh
+++ scripts/aws-scripts/validator_charlie-aws.sh
@@ -1,3 +1,3 @@
 rm -r -f /tmp/bob/
-/polkadex-aura-node/prometheus/prometheus --config.file /polkadex-aura-node/prometheus/prometheus.yml &
-/polkadex-aura-node/target/release/node-template --base-path /tmp/bob --chain /polkadex-aura-node/scripts/customSpecRaw.json --charlie --validator --ws-port 9955 --port 30333 --node-key 0000000000000000000000000000000000000000000000000000000000000003 --execution Native --prometheus-external --bootnodes /dns/balice-dev.polkadex.intra/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
+/polkadex/prometheus/prometheus --config.file /polkadex/prometheus/prometheus.yml &
+/polkadex/target/release/node-template --base-path /tmp/bob --chain /polkadex/scripts/customSpecRaw.json --charlie --validator --ws-port 9955 --port 30333 --node-key 0000000000000000000000000000000000000000000000000000000000000003 --execution Native --prometheus-external --bootnodes /dns/balice-dev.polkadex.intra/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
