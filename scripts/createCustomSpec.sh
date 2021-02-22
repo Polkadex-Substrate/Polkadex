5156793a9693256a8519111a41c3c9b98877146e
Pranayagaram <50867481+Pranayagaram@users.noreply.github.com>
2021-02-22 11:45:35 +0530

    updated the CCS.sh spec file 1.2

--- scripts/createCustomSpec.sh
+++ scripts/createCustomSpec.sh
@@ -1,2 +1,2 @@
-/polkadex-aura-node/target/release/node-polkadex build-spec  --chain local > customSpec.json
-/polkadex-aura-node/target/release/node-polkadex build-spec --chain=customSpec.json --raw > customSpecRaw.json
+/polkadex-aura-node/target/release/node-template build-spec  --chain local > customSpec.json
+/polkadex-aura-node/target/release/node-template build-spec --chain=customSpec.json --raw > customSpecRaw.json
