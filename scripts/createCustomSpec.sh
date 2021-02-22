7d0820aea6ab35153e600ad2af4e1d87a76006fe
Pranayagaram <50867481+Pranayagaram@users.noreply.github.com>
2021-02-22 17:15:09 +0530

    updated spec files 1.7

--- scripts/createCustomSpec.sh
+++ scripts/createCustomSpec.sh
@@ -1,2 +1,2 @@
-/polkadex/target/release/node-template build-spec  --chain local > /polkadex/scripts/customSpec.json
-/polkadex/target/release/node-template build-spec --chain=customSpec.json --raw > /polkadex/scripts/customSpecRaw.json
+/polkadex/target/release/node-polkadex build-spec  --chain local > /polkadex/scripts/customSpec.json
+/polkadex/target/release/node-polkadex build-spec --chain=customSpec.json --raw > /polkadex/scripts/customSpecRaw.json
