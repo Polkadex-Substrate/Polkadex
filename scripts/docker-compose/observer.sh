01a0edcee42f97ef080b5359b5e73eed31c84fd1
pranayagaram <pranay.agaram@people10.com>
2021-02-24 04:38:19 +0000

    added docker compose file

---
+++ scripts/docker-compose/observer.sh
@@ -0,0 +1,1 @@
+/polkadex/target/release/node-polkadex --chain /polkadex/scripts/customSpecRaw.json --dave --port 30333 --ws-port 9955 --rpc-port 9956 --ws-external --node-key 0000000000000000000000000000000000000000000000000000000000000005 --bootnodes /dns/bootnode_alice/tcp/30444/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp --prometheus-external
