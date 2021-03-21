7a31182ccac1acb520076efe66b4ffb598163576
ksr30 <krishna.singh9926@gmail.com>
2021-03-21 18:10:11 +0530

    Added metadata disptc function

--- pallets/Polkadex_Fungible_Assets/src/lib.rs
+++ pallets/Polkadex_Fungible_Assets/src/lib.rs
@@ -1,13 +1,14 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_support::{
-    decl_error, decl_event, decl_module, dispatch::DispatchResult,
+    decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult,
     ensure,
 };
 use frame_system as system;
 use frame_system::ensure_signed;
 use sp_runtime::traits::Zero;
 use sp_std::prelude::*;
+use codec::{Decode, Encode};
 
 #[cfg(test)]
 mod mock;
@@ -17,6 +18,38 @@ mod test;
 
 pub trait Config: system::Config + orml_tokens::Config {
     type Event: From<Event<Self>> + Into<<Self as system::Config>::Event>;
+}
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug)]
+pub struct AssetMetadata {
+    name: Vec<u8>,
+    website: Vec<u8>,
+    team: Vec<u8>
+}
+
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug)]
+pub struct AssetInfo<T: Config> {
+    is_mintable: Option<T::AccountId>,
+    is_burnable: Option<T::AccountId>,
+    metadata: Option<AssetMetadata>,
+    is_verified: bool
+}
+
+impl<T: Config> Default for AssetInfo<T> {
+    fn default() -> Self {
+        AssetInfo {
+            is_mintable: None,
+            is_burnable: None,
+            metadata: None,
+            is_verified: false
+        }
+    }
+}
+
+decl_storage! {
+    trait Store for Module<T: Config> as PolkadexFungible {
+        /// Stores AssetInfo
+        InfoAsset get(fn get_infoasset): map hasher(identity) T::CurrencyId => AssetInfo<T>;
+    }
 }
 
 decl_event!(
@@ -32,7 +65,8 @@ decl_event!(
 
 decl_error! {
 	pub enum Error for Module<T: Config> {
-		AssetIdAlreadyExists
+		AssetIdAlreadyExists,
+		AssetIdNotExists
 	}
 }
 
@@ -57,5 +91,25 @@ decl_module! {
                         Self::deposit_event(RawEvent::TokenIssued(asset_id, who, max_supply));
 			Ok(())
 		}
+
+        /// Info Fungible
+        #[weight = 10000]
+		pub fn set_metadata_fungible(origin, asset_id: T::CurrencyId, metadata: AssetMetadata) -> DispatchResult {
+		    let who: T::AccountId = ensure_signed(origin)?;
+		    // TODO: Ask @Gautam regarding who can add metadata
+		    ensure!(<InfoAsset<T>>::contains_key(&asset_id), <Error<T>>::AssetIdNotExists);
+		    InfoAsset::<T>::try_mutate(&asset_id, |ref mut asset_info| {
+		        asset_info.metadata = Some(metadata);
+		        Ok(())
+		    })
+		}
+
 	}
 }
+
+/// Questions
+/// Who can call metadata function?
+/// Who can be mintable and bunrable account
+/// deposit a fixed amount of Polkadex token to Polkadex treasury :: Explain
+/// Where to set exsistential deposit
+///
