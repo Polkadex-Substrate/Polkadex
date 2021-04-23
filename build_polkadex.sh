4b2ed71f2ed3faf5cc6c7ec5b3e80d4c8254eefa
ksr30 <krishna.singh9926@gmail.com>
2021-04-23 16:52:41 +0530

    update to 3.0.0

--- build_polkadex.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$1" = "mainnet" ]; then
-  cargo build --manifest-path bin/polkadex/Cargo.toml --features mainnet
-else
-  cargo build --manifest-path bin/polkadex/Cargo.toml --features testnet
-fi
