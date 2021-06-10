e334a53b5dc0d50972763f17ab08732014f5dbf3
mambisi <lilbizi96@gmail.com>
2021-06-10 15:31:30 +0000

    remove node primitive use polkadex primitive

--- node/src/service.rs
+++ node/src/service.rs
@@ -613,7 +613,7 @@ mod tests {
     use codec::Encode;
     use node_polkadex_runtime::constants::{currency::CENTS, time::SLOT_DURATION};
     use node_polkadex_runtime::{Address, BalancesCall, Call, UncheckedExtrinsic};
-    use node_primitives::{Block, DigestItem, Signature};
+    use polkadex_primitives::{Block, DigestItem, Signature};
     use sc_client_api::BlockBackend;
     use sc_consensus_babe::{BabeIntermediate, CompatibleDigestItem, INTERMEDIATE_KEY};
     use sc_consensus_epochs::descendent_query;
--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -19,7 +19,7 @@
 
 /// Money matters.
 pub mod currency {
-    use node_primitives::Balance;
+    use polkadex_primitives::Balance;
 
     pub const MILLICENTS: Balance = 1_000_000_000;
     pub const CENTS: Balance = 1_000 * MILLICENTS;
@@ -33,7 +33,7 @@ pub mod currency {
 
 /// Time.
 pub mod time {
-    use node_primitives::{BlockNumber, Moment};
+    use polkadex_primitives::{BlockNumber, Moment};
 
     /// Since BABE is probabilistic this is the average expected block time that
     /// we are targeting. Blocks will be produced at a minimum duration defined
