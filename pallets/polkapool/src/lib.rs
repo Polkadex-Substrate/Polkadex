ce72e6dd478776f9fb64abb751f44d10802e04c2
Gautham J <Gauthamastro@gmail.com>
2021-06-21 15:10:44 +0530

    Encode, Decode issue

--- pallets/polkapool/src/lib.rs
+++ pallets/polkapool/src/lib.rs
@@ -6,7 +6,7 @@
 use frame_support::{
     decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get,
 };
-use frame_support::dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo, UnfilteredDispatchable};
+use frame_support::dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo, UnfilteredDispatchable, EncodeLike};
 use frame_support::pallet_prelude::*;
 use frame_support::sp_runtime::traits::AtLeast32BitUnsigned;
 use frame_support::traits::{IsSubType, Randomness};
@@ -45,19 +45,20 @@ pub trait Config: frame_system::Config {
     + GetDispatchInfo + From<frame_system::Call<Self>>
     + UnfilteredDispatchable<Origin=Self::Origin>
     + IsSubType<Call<Self>>
-    + IsType<<Self as frame_system::Config>::Call>;
+    + IsType<<Self as frame_system::Config>::Call>
+    + Encode + Decode;
     /// Randomness Source
     type RandomnessSource: Randomness<H256, BlockNumber>;
 }
 
-#[derive(Decode, Encode, Default)]
-pub struct StakeInfo<T: Config> {
-    pub staked_amount: T::Balance,
-    pub unlocking_block: T::BlockNumber,
+#[derive(Decode, Encode, Default, Copy, Clone)]
+pub struct StakeInfo<Balance, BlockNumber> {
+    pub staked_amount: Balance,
+    pub unlocking_block: BlockNumber,
 }
 
-impl<T: Config> StakeInfo<T> {
-    pub fn new(stake: T::Balance, unlock: T::BlockNumber) -> StakeInfo<T> {
+impl<Balance, BlockNumber> StakeInfo<Balance, BlockNumber> {
+    pub fn new(stake: Balance, unlock: BlockNumber) -> StakeInfo<Balance, BlockNumber> {
         StakeInfo {
             staked_amount: stake,
             unlocking_block: unlock,
@@ -65,45 +66,57 @@ impl<T: Config> StakeInfo<T> {
     }
 }
 
-#[derive(Decode, Encode, Default)]
-pub struct MovingAverage<T: Config> {
-    pub amount: T::Balance,
-    pub count: T::Balance,
+#[derive(Decode, Encode, Default, Copy, Clone)]
+pub struct MovingAverage<Balance> {
+    pub amount: Balance,
+    pub count: Balance,
 }
 
-impl<T: Config> MovingAverage<T> {
-    pub fn update_stake_amount(&mut self, stake_amount: &T::Balance) {
+impl<Balance> MovingAverage<Balance> {
+    pub fn update_stake_amount(&mut self, stake_amount: &Balance) {
         self.amount = ((self.amount * self.count) + *stake_amount) / (self.count + 1u128.saturated_into());
         self.count += 1u128.saturated_into();
     }
 }
 
-#[derive(Decode, Encode, Default)]
-pub struct Ext<T: Config> {
-    pub call: <T as Config>::Call,
-    pub origin: T::Origin,
+#[derive(Decode, Encode, Default, Copy, Clone)]
+pub struct Ext<Call, Origin> {
+    pub call: Call,
+    pub origin: Origin,
 }
 
-#[derive(Decode, Encode, Default)]
-pub struct ExtStore<T: Config> {
+// impl<Call, Origin> Default for Ext<Call, Origin> {
+//     fn default() -> Self {
+//         todo!()
+//     }
+// }
+
+#[derive(Decode, Encode, Default, Clone)]
+pub struct ExtStore<Call, Origin> {
     /// vector of eligible feeless extrinsics
-    pub store: Vec<Ext<T>>,
+    pub store: Vec<Ext<Call, Origin>>,
     /// Total Weight of the stored extrinsics
     pub total_weight: Weight,
 }
+
+// impl<Call, Origin> Default for ExtStore<Call, Origin> {
+//     fn default() -> Self {
+//         todo!()
+//     }
+// }
 
 decl_storage! {
     trait Store for Module<T: Config> as Polkapool {
         /// All users and their staked amount
         /// (when they can claim, accountId => Balance)
-        pub StakedUsers get(fn staked_users):  map hasher(blake2_128_concat) T::AccountId => StakeInfo<T>;
+        pub StakedUsers get(fn staked_users):  map hasher(blake2_128_concat) T::AccountId => StakeInfo<T::Balance,T::BlockNumber>;
 
         /// StakeMovingAverage
         /// TODO: Set StakeMovingAverage as MinStakeAmount if it is zero
-        pub StakeMovingAverage get(fn get_stake_moving_average): MovingAverage<T>;
+        pub StakeMovingAverage get(fn get_stake_moving_average): MovingAverage<T::Balance>;
 
         /// Feeless Extrinsics stored for next block
-        pub TxnsForNextBlock get(fn get_next_block_txns): ExtStore<T>;
+        pub TxnsForNextBlock get(fn get_next_block_txns): ExtStore<<T as Config>::Call, T::Origin>;
     }
 }
 
@@ -139,7 +152,7 @@ decl_module! {
 
         fn on_initialize(n: T::BlockNumber) -> Weight {
             // Load the exts and clear the storage
-            let stored_exts: ExtStore<T> = <TxnsForNextBlock<T>>::take();
+            let stored_exts: ExtStore<<T as Config>::Call, T::Origin> = <TxnsForNextBlock<T>>::take();
             // TODO: Randomize the vector using babe randomness
             let mut used_weight: Weight = Weight::from(0);
             // Start executing
@@ -156,18 +169,18 @@ decl_module! {
             ensure!(stake_amount >= T::MinStakeAmount::get(), Error::<T>::StakeAmountTooSmall);
             ensure!(stake_amount <= T::Currency::free_balance(AssetId::POLKADEX,&who), Error::<T>::NotEnoughBalanceToStake);
 
-            let mut stored_exts: ExtStore<T> = Self::get_next_block_txns();
+            let mut stored_exts: ExtStore<<T as Config>::Call, T::Origin> = Self::get_next_block_txns();
             ensure!(stored_exts.store.len() < T::MaxAllowedTxns::get(), Error::<T>::NoMoreFeelessTxnsForThisBlock);
 
             // Update the moving average of stake amount
-            let mut stake_moving_average: MovingAverage<T> = Self::get_stake_moving_average();
+            let mut stake_moving_average: MovingAverage<T::Balance> = Self::get_stake_moving_average();
             stake_moving_average.update_stake_amount(&stake_amount);
 
             // Get current block number
             let current_block_number: T::BlockNumber = <frame_system::Pallet<T>>::block_number();
 
             // Store the staking record
-            let mut staked_amount: StakeInfo<T> = Self::staked_users(who.clone());
+            let mut staked_amount: StakeInfo<T::Balance,T::BlockNumber> = Self::staked_users(who.clone());
             staked_amount.staked_amount += stake_amount;
             staked_amount.unlocking_block = current_block_number + T::MinStakePeriod::get();
 
