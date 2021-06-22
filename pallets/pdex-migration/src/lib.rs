8b4ee11d8a3193487d328ec5947d28263d56905d
Gautham <Gauthamastro@gmail.com>
2021-06-22 11:31:59 +0530

    ERC20 to Native PDEX Migration (#204)
    * Import Ethereum Lightclient verifier
    
    * Import Ethereum Lightclient verifier
    
    * Almost Working
    
    * Implements ERC20 to Native PDEX migration
    
    * feat: migrates ERC20 to Native PDEX

---
+++ pallets/pdex-migration/src/lib.rs
@@ -0,0 +1,91 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// https://substrate.dev/docs/en/knowledgebase/runtime/frame
+use frame_support::{
+    decl_error, decl_event, decl_module, decl_storage,
+};
+use frame_support::pallet_prelude::*;
+use frame_support::sp_runtime::SaturatedConversion;
+use frame_support::sp_runtime::traits::AtLeast32BitUnsigned;
+use orml_traits::{MultiCurrency, MultiCurrencyExtended};
+use polkadex_primitives::assets::AssetId;
+use sp_core::{H160, U256};
+use sp_runtime::traits::StaticLookup;
+
+
+/// Configure the pallet by specifying the parameters and types on which it depends.
+pub trait Config: frame_system::Config {
+    /// Because this pallet emits events, it depends on the runtime's definition of an event.
+    type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
+    /// Balance Type
+    type Balance: Parameter
+    + Member
+    + AtLeast32BitUnsigned
+    + Default
+    + Copy
+    + MaybeSerializeDeserialize;
+    /// Module that handles tokens
+    type Currency: MultiCurrencyExtended<
+        Self::AccountId,
+        CurrencyId=AssetId,
+        Balance=Self::Balance,
+    >;
+
+    type CallOrigin: EnsureOrigin<Self::Origin, Success=H160>;
+}
+
+decl_storage! {
+	trait Store for Module<T: Config> as NativePDEXMigration {
+		/// Address of ERC20 to Native PDEX migration contract
+		Address get(fn address) config(): H160;
+	}
+}
+
+// Pallets use events to inform users when important changes are made.
+// https://substrate.dev/docs/en/knowledgebase/runtime/events
+decl_event!(
+    pub enum Event<T>
+    where
+        AccountId = <T as frame_system::Config>::AccountId,
+        Balance = <T as Config>::Balance,
+    {
+        NativePDEXMinted(H160, H160, AccountId, U256, Balance),
+    }
+);
+
+// Errors inform users that something went wrong.
+decl_error! {
+    pub enum Error for Module<T: Config> {
+        /// The submitted payload could not be decoded.
+		InvalidPayload,
+    }
+}
+
+// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+// These functions materialize as "extrinsics", which are often compared to transactions.
+// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+decl_module! {
+    pub struct Module<T: Config> for enum Call where origin: T::Origin {
+        // Errors must be initialized if they are used by the pallet.
+        type Error = Error<T>;
+
+        fn deposit_event() = default;
+
+        #[weight = 10000]
+		pub fn mint(origin, token: H160, sender: H160, recipient: <T::Lookup as StaticLookup>::Source, amount: U256) -> DispatchResult {
+			let who = T::CallOrigin::ensure_origin(origin)?;
+			if who != Address::get() {
+				return Err(DispatchError::BadOrigin.into());
+			}
+
+			let recipient = T::Lookup::lookup(recipient)?;
+            // TODO: Convert U256 amount to T::Balance amount
+			// T::Currency::deposit(AssetId::POLKADEX, &recipient, amount)?;
+			Self::deposit_event(RawEvent::NativePDEXMinted(token, sender, recipient, amount,0_u128.saturated_into()));
+
+			Ok(())
+		}
+    }
+}
