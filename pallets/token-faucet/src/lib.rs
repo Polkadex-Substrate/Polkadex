1189583f592c68030da067cda9575880fcd7d63e
ksr30 <krishna.singh9926@gmail.com>
2021-06-29 13:29:36 +0530

    Resolved merge conflict

--- pallets/token-faucet/src/lib.rs
+++ pallets/token-faucet/src/lib.rs
@@ -7,13 +7,12 @@ use frame_support::sp_runtime::SaturatedConversion;
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 use frame_support::{
-    debug, decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get,
+    decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get,
 };
 use frame_system::ensure_none;
 use orml_traits::{MultiCurrency, MultiCurrencyExtended};
 
 use polkadex_primitives::assets::AssetId;
-use sp_runtime::print;
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Config: frame_system::Config {
@@ -76,7 +75,7 @@ decl_module! {
             let _ = ensure_none(origin)?;
             TokenFaucetMap::<T>::insert(&account,<frame_system::Pallet<T>>::block_number());
             //Mint account with free tokens
-            T::Currency::deposit(AssetId::POLKADEX, &account,(100000 as u128).saturated_into())?;
+            T::Currency::deposit(AssetId::POLKADEX, &account,(1000000000000000000 as u128).saturated_into())?;
             Self::deposit_event(RawEvent::AccountCredited(account));
             Ok(())
         }
