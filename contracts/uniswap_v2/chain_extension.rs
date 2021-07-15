2f9294829c0c49240949e7a404a98a9067962cb6
Frank Li <frankli.consult@gmail.com>
2021-07-14 23:29:33 -0400

    TokenAddress def

---
+++ contracts/uniswap_v2/chain_extension.rs
@@ -0,0 +1,45 @@
+use ink_env::Environment;
+use ink_lang as ink;
+
+/// Define the operations to interact with the substrate runtime
+#[ink::chain_extension]
+pub trait CurrencyExtension {
+    type ErrorCode = ChainExtensionError;
+
+    #[ink(extension = 0, returns_result = false)]
+    fn transfer() -> <CustomEnvironment as Environment>::Balance;
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode, err_derive::Error)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum ChainExtensionError {
+    #[error(display = "Transfer failed")]
+    TransferFailed,
+}
+
+impl ink_env::chain_extension::FromStatusCode for ChainExtensionError {
+    fn from_status_code(status_code: u32) -> Result<(), Self> {
+        match status_code {
+            0 => Ok(()),
+            1 => Err(Self::TransferFailed),
+            _ => panic!("encountered unknown status code"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub struct CustomEnvironment;
+
+impl Environment for CustomEnvironment {
+    const MAX_EVENT_TOPICS: usize =
+        <ink_env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
+
+    type AccountId = <ink_env::DefaultEnvironment as Environment>::AccountId;
+    type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
+    type Hash = <ink_env::DefaultEnvironment as Environment>::Hash;
+    type BlockNumber = <ink_env::DefaultEnvironment as Environment>::BlockNumber;
+    type Timestamp = <ink_env::DefaultEnvironment as Environment>::Timestamp;
+
+    type ChainExtension = CurrencyExtension;
+}
