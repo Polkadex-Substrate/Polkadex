42924c491233d5de553493c5187ba869d19acb42
Fredrik Simonsson <simson@polkadex.trade>
2021-09-10 17:15:38 +0900

    Added simple script

---
+++ scripts/benchmark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in  frame_system pallet pallet_babe pallet_balances pallet_bounties pallet_collective pallet_elections_phragmen pallet_grandpa pallet_identity pallet_im_online pallet_indices pallet_membership pallet_multisig pallet_proxy pallet_scheduler pallet_session pallet_staking pallet_timestamp pallet_treasury pallet_utility
+do
+   echo Try $i
+   ./target/release/polkadex-node benchmark   --chain soba  --pallet=$i --extrinsic=\* 
+done
