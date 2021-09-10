c88e42f9cfb300be68088fa0547dc1aeb07af406
Fredrik Simonsson <simson@polkadex.trade>
2021-09-10 14:34:40 +0900

    Compiles but Error: Input("No benchmarks found which match your input.")

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1215,10 +1215,48 @@ impl_runtime_apis! {
 
     #[cfg(feature = "runtime-benchmarks")]
     impl frame_benchmarking::Benchmark<Block> for Runtime {
-        fn benchmark_metadata(_: bool) -> (
+        fn benchmark_metadata(extra: bool) -> (
 			Vec<frame_benchmarking::BenchmarkList>,
 			Vec<frame_support::traits::StorageInfo>) {
-             todo!()
+            use frame_benchmarking::{list_benchmark, Benchmarking, BenchmarkList};
+            use frame_support::traits::StorageInfoTrait;
+
+            // Trying to add benchmarks directly to the Session Pallet caused cyclic dependency
+            // issues. To get around that, we separated the Session benchmarks into its own crate,
+            // which is why we need these two lines below.
+            // use pallet_session_benchmarking::Pallet as SessionBench;
+            // use pallet_offences_benchmarking::Pallet as OffencesBench;
+            use frame_system_benchmarking::Pallet as SystemBench;
+
+            let mut list = Vec::<BenchmarkList>::new();
+    
+            // list_benchmark!(list,extra, pallet_assets, Assets);
+            list_benchmark!(list,extra, pallet_babe, Babe);
+            list_benchmark!(list,extra, pallet_balances, Balances);
+            list_benchmark!(list,extra, pallet_bounties, Bounties);
+            list_benchmark!(list,extra, pallet_collective, Council);
+            // list_benchmark!(list,extra, pallet_election_provider_multi_phase, ElectionProviderMultiPhase);
+            list_benchmark!(list,extra, pallet_elections_phragmen, Elections);
+            list_benchmark!(list,extra, pallet_grandpa, Grandpa);
+            list_benchmark!(list,extra, pallet_identity, Identity);
+            list_benchmark!(list,extra, pallet_im_online, ImOnline);
+            list_benchmark!(list,extra, pallet_indices, Indices);
+            list_benchmark!(list,extra, pallet_membership, TechnicalMembership);
+            list_benchmark!(list,extra, pallet_multisig, Multisig);
+            // list_benchmark!(list,extra, pallet_offences, OffencesBench::<Runtime>);
+            list_benchmark!(list,extra, pallet_proxy, Proxy);
+            list_benchmark!(list,extra, pallet_scheduler, Scheduler);
+            // list_benchmark!(list,extra, pallet_session, SessionBench::<Runtime>);
+            list_benchmark!(list,extra, pallet_staking, Staking);
+            list_benchmark!(list,extra, frame_system, SystemBench::<Runtime>);
+            list_benchmark!(list,extra, pallet_timestamp, Timestamp);
+            list_benchmark!(list,extra, pallet_treasury, Treasury);
+            list_benchmark!(list,extra, pallet_utility, Utility);
+            list_benchmark!(list,extra, pallet_vesting, Vesting);
+            // list_benchmark!(list,extra, pallet_verifier_lightclient, VerifierLightclient);
+            let storage_info = AllPalletsWithSystem::storage_info();
+
+			return (list, storage_info)
         }
         fn dispatch_benchmark(
             config: frame_benchmarking::BenchmarkConfig
