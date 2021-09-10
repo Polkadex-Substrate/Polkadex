f32ea41d702f1d2048feaabffc8024004a426e54
Cem Eliguzel <cemeliguzel@gmail.com>
2021-09-10 05:44:36 +0000

    Fixes formatting

--- runtime/build.rs
+++ runtime/build.rs
@@ -1,9 +1,9 @@
 use substrate_wasm_builder::WasmBuilder;
 
 fn main() {
-    WasmBuilder::new()
-        .with_current_project()
-        .import_memory()
-        .export_heap_base()
-        .build()
+	WasmBuilder::new()
+		.with_current_project()
+		.import_memory()
+		.export_heap_base()
+		.build()
 }
