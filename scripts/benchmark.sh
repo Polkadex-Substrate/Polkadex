36aa688c0e69bd8db5f6ed20cba43776baaec8ba
Fredrik Simonsson <simson@polkadex.trade>
2021-09-10 17:21:47 +0900

    Improved script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+install -d benchout
 for i in  frame_system pallet pallet_babe pallet_balances pallet_bounties pallet_collective pallet_elections_phragmen pallet_grandpa pallet_identity pallet_im_online pallet_indices pallet_membership pallet_multisig pallet_proxy pallet_scheduler pallet_session pallet_staking pallet_timestamp pallet_treasury pallet_utility
 do
    echo Try $i
-   ./target/release/polkadex-node benchmark   --chain soba  --pallet=$i --extrinsic=\* 
+   ./target/release/polkadex-node benchmark   --chain soba  --pallet=$i --extrinsic=\*  --output=benchout/$i
 done
