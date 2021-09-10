bbe482f717aeed7eac3566c6b780d8801ebb68f7
Cem Eliguzel <cemeliguzel@gmail.com>
2021-09-10 14:46:10 +0000

    Fixes the benchmark running problem

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 
 install -d benchout
-for i in  frame_system pallet pallet_babe pallet_balances pallet_bounties pallet_collective pallet_elections_phragmen pallet_grandpa pallet_identity pallet_im_online pallet_indices pallet_membership pallet_multisig pallet_proxy pallet_scheduler pallet_session pallet_staking pallet_timestamp pallet_treasury pallet_utility
-do
+for i in frame_system pallet_babe pallet_balances pallet_bounties pallet_collective pallet_elections_phragmen pallet_grandpa pallet_identity pallet_im_online pallet_indices pallet_membership pallet_multisig pallet_proxy pallet_scheduler pallet_staking pallet_timestamp pallet_treasury pallet_utility; do
    echo Try $i
-   ./target/release/polkadex-node benchmark   --chain soba  --pallet=$i --extrinsic=\*  --output=benchout/$i
+   ./target/release/polkadex-node benchmark \
+      --chain dev \
+      --execution wasm \
+      --wasm-execution compiled \
+      --pallet=$i \
+      --extrinsic="*" \
+      --steps 1 \
+      --repeat 2 \
+      --output=benchout/$i
 done
