5c7e2bc14a75f19d36986f8412481f3bcb3384c1
Fredrik Simonsson <simson@polkadex.trade>
2021-09-12 15:11:24 +0900

    Update sha

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
+cargo build --release --features runtime-benchmarks
+./target/release/polkadex-node benchmark --chain dev --list
+
 
 install -d benchout
 for i in frame_system pallet_babe pallet_balances pallet_bounties pallet_collective pallet_elections_phragmen pallet_grandpa pallet_identity pallet_im_online pallet_indices pallet_membership pallet_multisig pallet_proxy pallet_scheduler pallet_session pallet_staking pallet_timestamp pallet_treasury pallet_utility; do
    echo Try $i
-   ./target/release/polkadex-node benchmark \
+   echo   ./target/release/polkadex-node benchmark \
       --chain dev \
       --execution wasm \
       --wasm-execution compiled \
