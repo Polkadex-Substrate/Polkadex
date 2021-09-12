dd02b6af8c3dfe9f44e70e5cf094841e45ab24dd
Fredrik Simonsson <simson@polkadex.trade>
2021-09-12 15:40:14 +0900

    Updated scripts and added orml

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -4,7 +4,8 @@ cargo build --release --features runtime-benchmarks
 
 
 install -d benchout
-for i in frame_system pallet_babe pallet_balances pallet_bounties pallet_collective pallet_elections_phragmen pallet_grandpa pallet_identity pallet_im_online pallet_indices pallet_membership pallet_multisig pallet_proxy pallet_scheduler pallet_session pallet_staking pallet_timestamp pallet_treasury pallet_utility; do
+#for i in frame_system pallet_babe pallet_balances pallet_bounties pallet_collective pallet_elections_phragmen pallet_grandpa pallet_identity pallet_im_online pallet_indices pallet_membership pallet_multisig pallet_proxy pallet_scheduler pallet_session pallet_staking pallet_timestamp pallet_treasury pallet_utility; do
+for i in `./target/release/polkadex-node benchmark --chain dev --list | sed s/,.*// |sort |uniq` ; do
    echo Try $i
    echo   ./target/release/polkadex-node benchmark \
       --chain dev \
@@ -12,7 +13,7 @@ for i in frame_system pallet_babe pallet_balances pallet_bounties pallet_collect
       --wasm-execution compiled \
       --pallet=$i \
       --extrinsic="*" \
-      --steps 1 \
-      --repeat 2 \
-      --output=benchout/$i
+      --steps 20 \
+      --repeat 50 \
+      --output=benchout/$i.rs
 done
