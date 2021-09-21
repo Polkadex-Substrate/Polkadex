24a2e9557d35ea43141c9e1385b5e1c76d7ee250
Fredrik Simonsson <simson@polkadex.trade>
2021-09-21 13:09:01 +0900

    Adding back removed mock

--- pallets/pdex-migration/src/benchmarking.rs
+++ pallets/pdex-migration/src/benchmarking.rs
@@ -5,15 +5,16 @@ use super::*;
 use frame_system::RawOrigin;
 use frame_benchmarking::{benchmarks,account, whitelisted_caller, impl_benchmark_test_suite};
 #[allow(unused)]
-use crate::pallet as PDEXMigration;
+// use crate::pallet as PDEXMigration;
+use crate::mock::{PDEXMigration,Origin,PDEX};
 
 use crate::pallet::{Config,Call,Pallet};
 use frame_support::traits::Get;
 use sp_runtime::traits::Saturating;
+use frame_support::assert_ok;
+// use frame_system::Origin;
+use sp_core::H256;
 
-// 3  | use crate::pallet::Pallet;
-//    |
-// 3  | use frame_system::Pallet;
 benchmarks! {
     set_migration_operational_status {
 
@@ -24,6 +25,19 @@ benchmarks! {
     }: _ (RawOrigin::Root, relayer, true)
 
     mint {
+        let relayer1 = account("relayer1",0,0);
+        let relayer2  = account("relayer2",0,0);
+        let relayer3 = account("relayer3",0,0);
+        let beneficiary = whitelisted_caller();
+        assert_ok!(PDEXMigration::set_migration_operational_status(Origin::root(),true));
+        // Register relayers
+        assert_ok!(PDEXMigration::set_relayer_status(Origin::root(),relayer1,true));
+        assert_ok!(PDEXMigration::set_relayer_status(Origin::root(),relayer2,true));
+        assert_ok!(PDEXMigration::set_relayer_status(Origin::root(),relayer3,true));
+
+        assert_ok!(PDEXMigration::mint(Origin::signed(relayer1), beneficiary,100*PDEX,H256::zero()));
+        assert_ok!(PDEXMigration::mint(Origin::signed(relayer2), beneficiary,100*PDEX,H256::zero()));
+
         let beneficiary: T::AccountId = whitelisted_caller();
         let eth_hash: T::Hash = T::Hash::default();
         let relayer3: T::AccountId = account("relayer3",0,0);
@@ -31,11 +45,40 @@ benchmarks! {
     }: _(RawOrigin::Signed(relayer3),beneficiary,amount,eth_hash)
 
     unlock {
-        
+        let relayer1 = account("relayer1",0,0);
+        let relayer2  = account("relayer2",0,0);
+        let relayer3 = account("relayer3",0,0);
+        let beneficiary = whitelisted_caller();
+        assert_ok!(PDEXMigration::set_migration_operational_status(Origin::root(),true));
+        // Register relayers
+        assert_ok!(PDEXMigration::set_relayer_status(Origin::root(),relayer1,true));
+        assert_ok!(PDEXMigration::set_relayer_status(Origin::root(),relayer2,true));
+        assert_ok!(PDEXMigration::set_relayer_status(Origin::root(),relayer3,true));
+
+        assert_ok!(PDEXMigration::mint(Origin::signed(relayer1), beneficiary,100*PDEX,H256::zero()));
+        assert_ok!(PDEXMigration::mint(Origin::signed(relayer2), beneficiary,100*PDEX,H256::zero()));
+        assert_ok!(PDEXMigration::mint(Origin::signed(relayer3), beneficiary,100*PDEX,H256::zero()));
+
+        frame_system::Pallet::<T>::set_block_number(frame_system::Pallet::<T>::current_block_number()+T::LockPeriod::get());
 
         let beneficiary: T::AccountId = whitelisted_caller();
     }: _(RawOrigin::Signed(beneficiary))
+
     remove_minted_tokens {
+        let relayer1 = account("relayer1",0,0);
+        let relayer2  = account("relayer2",0,0);
+        let relayer3 = account("relayer3",0,0);
+        let beneficiary = whitelisted_caller();
+        assert_ok!(PDEXMigration::set_migration_operational_status(Origin::root(),true));
+        // Register relayers
+        assert_ok!(PDEXMigration::set_relayer_status(Origin::root(),relayer1,true));
+        assert_ok!(PDEXMigration::set_relayer_status(Origin::root(),relayer2,true));
+        assert_ok!(PDEXMigration::set_relayer_status(Origin::root(),relayer3,true));
+
+        assert_ok!(PDEXMigration::mint(Origin::signed(relayer1), beneficiary,100*PDEX,H256::zero()));
+        assert_ok!(PDEXMigration::mint(Origin::signed(relayer2), beneficiary,100*PDEX,H256::zero()));
+        assert_ok!(PDEXMigration::mint(Origin::signed(relayer3), beneficiary,100*PDEX,H256::zero()));
+
         let beneficiary: T::AccountId = whitelisted_caller();
     }: _(RawOrigin::Root,beneficiary)
 }
