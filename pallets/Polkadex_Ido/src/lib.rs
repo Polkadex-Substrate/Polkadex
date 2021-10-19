8208d8aee2c31058dea5e3ef5c74ff09dc94699e
Mambisi Zempare <lilbizi96@gmail.com>
2021-10-19 13:20:03 +0000

    Feat: Configurable token price (#383)
    Configurable token price, The price of one sold token could be less than one source tokens
    
    Modification: min and max allocation is in TokenA
    `fn show_interest_in_round` check `amount_in_token_a` doesn't exceed min and max allocations

--- pallets/Polkadex_Ido/src/lib.rs
+++ pallets/Polkadex_Ido/src/lib.rs
@@ -60,7 +60,7 @@ use sp_runtime::traits::AccountIdConversion;
 use sp_runtime::traits::CheckedDiv;
 use sp_runtime::traits::Saturating;
 use sp_runtime::traits::Zero;
-use sp_runtime::{SaturatedConversion, Permill};
+use sp_runtime::{SaturatedConversion, Permill, Perbill};
 use sp_std::collections::btree_map::BTreeMap;
 use sp_std::collections::btree_set::BTreeSet;
 use sp_std::prelude::*;
@@ -226,6 +226,10 @@ impl<T: Config> FundingRound<T> {
             actual_raise: self.actual_raise.saturated_into(),
         }
     }
+
+    fn token_a_price_per_1e12_token_b(&self) -> Perbill {
+        Perbill::from_rational(self.token_a_priceper_token_b, 1_000_000_000_000_u128.saturated_into())
+    }
 }
 
 #[derive(Decode, Encode, Clone)]
@@ -308,7 +312,7 @@ decl_module! {
                     let mut funding_round = funding_round.clone();
                     for (investor_address, amount) in <InterestedParticipants<T>>::iter_prefix(round_id) {
                             <WhiteListInvestors<T>>::insert(round_id, investor_address.clone(), amount);
-                            let total_raise = funding_round.amount.saturating_mul(funding_round.token_a_priceper_token_b);
+                            let total_raise = funding_round.token_a_price_per_1e12_token_b().mul_floor(funding_round.amount);
                             let investor_share = Permill::from_rational_approximation(amount,total_raise);
                             let round_account_id = Self::round_account_id(round_id.clone());
 
@@ -418,9 +422,12 @@ decl_module! {
             let start_block = vote_end_block.clone().saturating_add(1_u128.saturated_into());
             let close_round_block = vote_end_block.saturating_add(funding_period);
 
+            // Ensures that
+            let token_a_priceper_token_b_perbill = Perbill::from_rational(token_a_priceper_token_b, 1_000_000_000_000_u128.saturated_into());
+
             // CID len must be less than or equal to 100
             ensure!(cid.len() <= 100, <Error<T>>::CidReachedMaxSize);
-            ensure!(token_a_priceper_token_b > 0_u128.saturated_into(), <Error<T>>::PricePerTokenCantBeZero);
+            ensure!(!token_a_priceper_token_b_perbill.is_zero(), <Error<T>>::PricePerTokenCantBeZero);
             ensure!(min_allocation <= max_allocation, <Error<T>>::MinAllocationMustBeEqualOrLessThanMaxAllocation);
             ensure!(start_block < close_round_block, <Error<T>>::StartBlockMustBeLessThanEndblock);
             ensure!(vote_end_block < start_block, <Error<T>>::StartBlockMustBeGreaterThanVotingPeriod);
@@ -542,9 +549,10 @@ decl_module! {
 
             //Check If investor can invest amount
             ensure!(T::Currency::ensure_can_withdraw(funding_round.token_b,&investor_address, amount).is_ok(), Error::<T>::BalanceInsufficientForInteresetedAmount);
-
+            // Max and Min allocation must be in token A to avoid the investor for under investing or over investing
+            let amount_in_token_a = funding_round.token_a_price_per_1e12_token_b().saturating_reciprocal_mul(amount);
             //Ensure investment amount doesn't exceed max_allocation
-            ensure!(amount <= funding_round.max_allocation && amount >= funding_round.min_allocation, Error::<T>::NotAValidAmount);
+            ensure!(amount_in_token_a <= funding_round.max_allocation && amount_in_token_a >= funding_round.min_allocation, Error::<T>::NotAValidAmount);
 
             let current_block_no = <frame_system::Pallet<T>>::block_number();
             ensure!(current_block_no >= funding_round.start_block && current_block_no < funding_round.close_round_block, <Error<T>>::NotAllowed);
@@ -702,7 +710,7 @@ decl_module! {
             let funding_round = <WhitelistInfoFundingRound<T>>::get(round_id);
             ensure!(creator.eq(&funding_round.creator), <Error<T>>::NotACreater);
             // Check if there is any left to withdraw
-            let total_tokens_bought_by_investors = funding_round.actual_raise.saturating_mul(1_u32.saturated_into::<T::Balance>()/ funding_round.token_a_priceper_token_b);
+            let total_tokens_bought_by_investors = funding_round.token_a_price_per_1e12_token_b().saturating_reciprocal_mul(funding_round.actual_raise);
             let remaining_token = funding_round.amount.saturating_sub(total_tokens_bought_by_investors);
             ensure!(current_block_no >= funding_round.close_round_block, Error::<T>::WithdrawalBlocked);
             ensure!(remaining_token > Zero::zero(), Error::<T>::WithdrawalBlocked);
--- pallets/Polkadex_Ido/src/test.rs
+++ pallets/Polkadex_Ido/src/test.rs
@@ -76,7 +76,7 @@ fn test_register_round() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -112,7 +112,7 @@ fn test_whitelist_investor() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -180,7 +180,7 @@ fn test_participate_in_round() {
                 funding_period,
                 min_allocation,
                 max_allocation,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -267,7 +267,7 @@ fn test_claim_tokens() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -295,6 +295,74 @@ fn test_claim_tokens() {
     });
 }
 
+#[test]
+fn test_claim_edge_case_tokens() {
+    let balance: Balance = 100;
+    let investor_address: u64 = 4;
+    let funding_period = 10;
+    let round_id = create_hash_data(&1u32);
+    let cid = [0_u8;32].to_vec();
+    ExtBuilder::default().build().execute_with(|| {
+        assert_noop!(
+            PolkadexIdo::claim_tokens(Origin::signed(investor_address), round_id,),
+            Error::<Test>::InvestorDoesNotExist
+        );
+
+        assert_eq!(
+            PolkadexIdo::register_investor(Origin::signed(investor_address)),
+            Ok(())
+        );
+
+        assert_noop!(
+            PolkadexIdo::claim_tokens(Origin::signed(investor_address), round_id,),
+            Error::<Test>::FundingRoundDoesNotExist
+        );
+
+
+        assert_eq!(
+            PolkadexIdo::register_round(
+                Origin::signed(ALICE),
+                cid,
+                Some(AssetId::Asset(24)),
+                balance,
+                AssetId::POLKADEX,
+                balance,
+                funding_period,
+                balance,
+                balance,
+                f64_to_balance(0.250),
+            ),
+            Ok(())
+        );
+
+        let round_id = <InfoProjectTeam<Test>>::get(ALICE.clone());
+        let funding_round = <InfoFundingRound<Test>>::get(&round_id);
+        let closing_block_number = funding_round.close_round_block;
+        let open_block_number = funding_round.start_block;
+        PolkadexIdo::approve_ido_round(Origin::signed(1_u64), round_id);
+        system::Pallet::<Test>::set_block_number(open_block_number);
+
+        // Investor invests 25 PDEX, will get 100% share (100 in Asset(24) tokens)  since 25 / 0.25 = 100
+        assert_eq!(
+            PolkadexIdo::show_interest_in_round(Origin::signed(investor_address), round_id, 25_u128.saturated_into()),
+            Ok(())
+        );
+        <PolkadexIdo as OnInitialize<u64>>::on_initialize(closing_block_number);
+        system::Pallet::<Test>::set_block_number(closing_block_number + 1);
+
+        assert_eq!(
+            PolkadexIdo::claim_tokens(Origin::signed(investor_address), round_id,),
+            Ok(())
+        );
+
+        // Test investor will get all available(100 Assets(24) tokens) token  Asset(24),
+        assert_eq!(
+            <Test as Config>::Currency::free_balance(AssetId::Asset(24),&investor_address ),
+            balance
+        );
+
+    });
+}
 #[test]
 fn test_show_interest_in_round() {
     let balance: Balance = 500;
@@ -332,7 +400,7 @@ fn test_show_interest_in_round() {
                 funding_period,
                 min_allocation,
                 max_allocation,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -392,7 +460,7 @@ fn test_show_interest_in_round_randomized_participants() {
                 funding_period,
                 min_allocation,
                 max_allocation,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -474,7 +542,7 @@ fn test_withdraw_raise() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -512,7 +580,7 @@ fn test_withdraw_raise() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -575,7 +643,7 @@ fn test_withdraw_token() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -613,7 +681,7 @@ fn test_withdraw_token() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -660,7 +728,7 @@ fn test_vote_for_round() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -711,7 +779,7 @@ fn test_vote_for_round_no_vote_majority() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
@@ -746,7 +814,11 @@ fn test_vote_for_round_no_vote_majority() {
         );
     });
 }
+pub const PDEX: Balance = 1_000_000_000_000;
 
+fn f64_to_balance(value : f64) -> Balance {
+    Permill::from_float(value).mul_ceil( PDEX)
+}
 
 /// Test whether the voter will receive amount when the vote stake period ends
 /// voter adds vote with amount
@@ -769,7 +841,7 @@ fn test_get_reserve_amount() {
                 funding_period,
                 balance,
                 balance,
-                10.saturated_into(),
+                f64_to_balance(10.0),
             ),
             Ok(())
         );
