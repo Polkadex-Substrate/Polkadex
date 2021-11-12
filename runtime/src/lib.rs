d274da30fb1e3bc53630d58ceff5a282d04bc22c
Frank Li <frankli.consult@gmail.com>
2021-11-12 07:14:16 -0500

    update withdraw functionality

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -892,7 +892,7 @@ mod impl_uniswap {
                         match <crate::Balances as Currency<AccountId>>::transfer(
                             &from,
                             &to,
-                            1,
+                            amount,
                             ExistenceRequirement::AllowDeath
                         ) {
                             Ok(res) => {
@@ -901,7 +901,6 @@ mod impl_uniswap {
                             }
                             Err(err) => {
                                 log::info!("ChainExtension error {:?}", err);
-                                panic!("ChainExtension error {:?}", err);
                                 return Err(DispatchError::Other(
                                     "ChainExtension failed to call transfer",
                                 ));
@@ -917,37 +916,60 @@ mod impl_uniswap {
                 1 => {
                     // withdraw
                     let mut env = env.buf_in_buf_out();
-                    let input_data = env.read(56)?;
-
-                    log::debug!("hello ~~ withdraw ~~");
+                    let input_data = env.read(57)?;
 
                     let asset_id: AssetId;
-                    let uvalue: u64 = u64::from_le_bytes(input_data[0..8].try_into().unwrap());
-                    log::debug!("{:?}", uvalue);
-                    if uvalue != 0 {
-                        asset_id = AssetId::Asset(0x1);
-                    } else {
-                        asset_id = AssetId::POLKADEX;
-                    }
+                    let uvalue: u64;
                     let from: AccountId = PalletId(*b"polkadex").into_account();
-                    let to: AccountId = AccountId::new(input_data[8..40].try_into().unwrap());
-                    log::debug!("-------------- {:?} {:?}", asset_id, to);
-                    let amount: Balance = u128::from_le_bytes(input_data[40..].try_into().unwrap());
-                    log::debug!("-------------- {:?} {:?} {:?}", asset_id, from, amount);
+                    let to: AccountId;
+                    let amount: Balance;
+                    let asset_type: u8 = u8::from_le_bytes(input_data[0..1].try_into().unwrap());
 
-                    match <crate::Currencies as MultiCurrency<AccountId>>::transfer(
-                        asset_id,
-                        &from,
-                        &to,
-                        1,
-                    ) {
-                        Ok(_) => {
-                            env.write(&[0], false, None)?;
+                    if asset_type != 0 {
+                        uvalue = u64::from_le_bytes(input_data[1..9].try_into().unwrap());
+                        asset_id = AssetId::Asset(uvalue);
+                        to = AccountId::new(input_data[9..41].try_into().unwrap());
+                        amount = u128::from_le_bytes(input_data[41..].try_into().unwrap());
+
+                        match <crate::Currencies as MultiCurrency<AccountId>>::transfer(
+                            asset_id,
+                            &from,
+                            &to,
+                            amount,
+                        ) {
+                            Ok(res) => {
+                                log::info!("ChainExtension success {:?}", res);
+                                env.write(&[0], false, None)?;
+                            }
+                            Err(err) => {
+                                log::info!("ChainExtension error {:?}", err);
+                                return Err(DispatchError::Other(
+                                    "ChainExtension failed to call transfer",
+                                ));
+                            }
                         }
-                        Err(_) => {
-                            return Err(DispatchError::Other(
-                                "ChainExtension failed to call transfer",
-                            ));
+                    } else {
+                        uvalue = 0;
+                        asset_id = AssetId::POLKADEX;
+                        to = AccountId::new(input_data[1..33].try_into().unwrap());
+                        amount = u128::from_le_bytes(input_data[33..49].try_into().unwrap());
+
+                        match <crate::Balances as Currency<AccountId>>::transfer(
+                            &from,
+                            &to,
+                            amount,
+                            ExistenceRequirement::AllowDeath
+                        ) {
+                            Ok(res) => {
+                                log::info!("ChainExtension success {:?}", res);
+                                env.write(&[0], false, None)?;
+                            }
+                            Err(err) => {
+                                log::info!("ChainExtension error {:?}", err);
+                                return Err(DispatchError::Other(
+                                    "ChainExtension failed to call transfer",
+                                ));
+                            }
                         }
                     }
                     env.write(&[0], false, None)?;
