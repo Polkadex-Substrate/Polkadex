4bd6326663bfb6ecce65ccafcea866899e5ccf0f
felixfaisal <faisalahmedfarooq46@gmail.com>
2022-06-02 18:06:37 +0530

    fix: add AssetManager and skeleton extrinsic

--- pallets/test-token-provider/src/lib.rs
+++ pallets/test-token-provider/src/lib.rs
@@ -3,22 +3,94 @@ pub mod pallet {
 	use codec::{Decode, Encode, MaxEncodedLen};
 	use frame_support::{
 		pallet_prelude::*,
-		traits::{fungible::Mutate, Currency, Get, LockableCurrency, WithdrawReasons},
+		traits::{
+			tokens::fungibles::{Create, Inspect, Mutate},
+			Currency, Get, LockableCurrency, WithdrawReasons,
+		},
 	};
 	use frame_system::pallet_prelude::*;
 	use scale_info::TypeInfo;
 	use sp_runtime::{
-		traits::{BlockNumberProvider, Saturating, Zero},
+		traits::{AtLeast32BitUnsigned, BlockNumberProvider, Saturating, Zero},
 		SaturatedConversion,
 	};
+	// use frame_support::traits::tokens::nonfungibles::Create;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		
+		/// The overarching event type.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		/// Responsible for minting tokens
+		type AssetManager: Create<<Self as frame_system::Config>::AccountId>
+			+ Mutate<<Self as frame_system::Config>::AccountId, Balance = u128, AssetId = u128>
+			+ Inspect<<Self as frame_system::Config>::AccountId>;
+		/// Balance Type
+		type Balance: Parameter
+			+ Member
+			+ AtLeast32BitUnsigned
+			+ Default
+			+ Copy
+			+ MaybeSerializeDeserialize;
 	}
 
 	#[pallet::pallet]
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(PhantomData<T>);
 
+	#[pallet::error]
+	pub enum Error<T> {
+		SignerNotFound,
+		OffenderNotFound,
+		BoundOverflow,
+	}
+
+	#[pallet::validate_unsigned]
+	impl<T: Config> frame_support::unsigned::ValidateUnsigned for Pallet<T> {
+		type Call = Call<T>;
+
+		fn validate_unsigned(_source: TransactionSource, call: &Self::Call) -> TransactionValidity {
+			let rng: u64 = 20;
+
+			ValidTransaction::with_tag_prefix("thea-proc")
+				.priority(rng)
+				.and_provides([&(rng.to_be())])
+				.longevity(3)
+				.propagate(true)
+				.build()
+		}
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight((10_000, DispatchClass::Normal))]
+		pub fn credit_account_with_tokens_unsigned(
+			origin: OriginFor<T>,
+			account: T::AccountId,
+		) -> dispatch::DispatchResult {
+			// Code here to mint tokens
+			Ok(())
+		}
+	}
+
+	/* #[pallet::storage]
+	#[pallet::getter(fn keygen_messages)]
+	/// sender, KeygenRound => Messages
+	pub(super) type KeygenMessages<T: Config> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		PartyIndex,
+		Blake2_128Concat,
+		KeygenRound,
+		TheaPayload<
+			T::TheaId,
+			KeygenRound,
+			thea_primitives::MsgLimit,
+			thea_primitives::MsgVecLimit,
+		>,
+		ValueQuery,
+	*/
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub (super) fn deposit_event)]
+	pub enum Event<T: Config> {}
 }
