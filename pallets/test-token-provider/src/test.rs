ea866887da411644591e17af8aa90819591e373a
Faisal Ahmed <42486737+felixfaisal@users.noreply.github.com>
2022-06-09 12:50:11 +0530

    Test token provider pallet for testnet  (#424)
    * add: skeleton pallet
    
    * fix: add AssetManager and skeleton extrinsic
    
    * add: demo minting function
    
    * add: mock runtime environment with dependencies
    
    * add: small mvp with unit tests
    
    * fix: integrate token provider with runtime
    
    * fix: validate_unsigned in token provider
    
    * fix: validation logic for minting tokens
    
    * fix: minting native token
    
    * refactor: update manifest
    
    * fix: asset_id for ether
    
    * fix: build errors due to deps
    
    * fix: hardcoded asset_id
    
    * fix: provide accout to resolve priority issue

---
+++ pallets/test-token-provider/src/test.rs
@@ -0,0 +1,33 @@
+use crate::mock::*;
+use codec::Encode;
+use frame_support::assert_ok;
+// use sp_application_crypto::RuntimePublic;
+use sp_core::sr25519::Signature;
+use std::convert::TryInto;
+use sp_keystore::{testing::KeyStore, KeystoreExt, SyncCryptoStore};
+use crate::pallet::Pallet;
+
+
+#[test]
+fn test_minting_token(){
+    const KEY_TYPE: sp_application_crypto::KeyTypeId = sp_application_crypto::KeyTypeId(*b"thea");
+    const PHRASE: &str =
+    "news slush supreme milk chapter athlete soap sausage put clutch what kitten";
+    let keystore = KeyStore::new();
+    let account_id = SyncCryptoStore::sr25519_generate_new(
+        &keystore,
+        KEY_TYPE,
+        Some(&format!("{}/hunter1", PHRASE)),
+    ).unwrap();
+
+    let account_id_2 = SyncCryptoStore::sr25519_generate_new(
+        &keystore,
+        KEY_TYPE,
+        Some(&format!("{}/hunter2", PHRASE)),
+    ).unwrap();
+    new_test_ext().execute_with(|| {
+        assert_ok!(Token::credit_account_with_tokens_unsigned(Origin::none(), account_id));
+        assert_ok!(Token::credit_account_with_tokens_unsigned(Origin::none(), account_id_2));
+        
+    });
+}
