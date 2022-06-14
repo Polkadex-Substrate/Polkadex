1fa3c173812fcda6e723fa27fd812e37a83d17a2
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-06-14 13:28:51 +0530

    fix: include ido rpc in node

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -113,6 +113,7 @@ where
 	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
 	C::Api: BabeApi<Block>,
 	C::Api: BlockBuilder<Block>,
+	C::Api: polkadex_ido_rpc::PolkadexIdoRuntimeApi<Block, AccountId, Hash>,
 	P: TransactionPool + 'static,
 	SC: SelectChain<Block> + 'static,
 	B: sc_client_api::Backend<Block> + Send + Sync + 'static,
@@ -156,14 +157,21 @@ where
 		finality_provider,
 	)));
 
+	
 	io.extend_with(sc_sync_state_rpc::SyncStateRpcApi::to_delegate(
 		sc_sync_state_rpc::SyncStateRpcHandler::new(
 			chain_spec,
-			client,
+			client.clone(),
 			shared_authority_set,
 			shared_epoch_changes,
 		)?,
 	));
+
+	io.extend_with(polkadex_ido_rpc::PolkadexIdoRpcApi::to_delegate(
+		polkadex_ido_rpc::PolkadexIdoRpc::new(client),
+	));
+
+	
 
 	Ok(io)
 }
