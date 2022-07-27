6a7c40bc22677cf3eb5ff56754b28c1dd53f32bc
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-07-27 18:58:25 +0530

    fix: add unit tests for extrinsics

--- pallets/ocex/src/tests.rs
+++ pallets/ocex/src/tests.rs
@@ -23,7 +23,7 @@ use frame_support::{
 	assert_noop, assert_ok,
 };
 use frame_support::traits::OnTimestampSet;
-use polkadex_primitives::{Moment, Signature};
+use polkadex_primitives::{Moment, Signature, assets::AssetId};
 use sp_std::cell::RefCell;
 use frame_system::EnsureRoot;
 use sp_core::H256;
@@ -44,11 +44,120 @@ fn test_register_main_account(){
 	let account_id = create_account_id();
 
 	new_test_ext().execute_with(|| {
+		assert_eq!(Accounts::<Test>::contains_key::<AccountId32>(account_id.clone().into()), false);
 		assert_ok!(OCEX::register_main_account(Origin::signed(account_id.clone().into()), account_id.clone().into()));
 		assert_eq!(Accounts::<Test>::contains_key::<AccountId32>(account_id.into()), true);
 	});
 }
 
+#[test]
+fn test_register_main_account_main_account_already_exists(){
+	let account_id = create_account_id();
+
+	new_test_ext().execute_with(|| {
+		assert_ok!(OCEX::register_main_account(Origin::signed(account_id.clone().into()), account_id.clone().into()));
+		assert_eq!(Accounts::<Test>::contains_key::<AccountId32>(account_id.clone().into()), true);
+		assert_noop!(OCEX::register_main_account(Origin::signed(account_id.clone().into()), account_id.clone().into()), Error::<Test>::MainAccountAlreadyRegistered);
+	});
+}
+
+#[test]
+fn test_add_proxy_account_main_account_not_found(){
+	let account_id = create_account_id(); 
+
+	new_test_ext().execute_with(|| {
+		assert_noop!(
+			OCEX::add_proxy_account(Origin::signed(account_id.clone().into()), account_id.into()),
+			Error::<Test>::MainAccountNotFound
+		);
+	});
+}
+
+#[test]
+fn test_add_proxy_account(){
+	let account_id = create_account_id(); 
+
+	new_test_ext().execute_with(|| {
+		assert_ok!(OCEX::register_main_account(Origin::signed(account_id.clone().into()), account_id.clone().into()));
+		assert_ok!(OCEX::add_proxy_account(Origin::signed(account_id.clone().into()), account_id.clone().into()));
+	});
+}
+
+#[test]
+fn test_register_trading_pair_both_assets_cannot_be_same(){
+	new_test_ext().execute_with(||{
+		assert_noop!(
+			OCEX::register_trading_pair(
+				Origin::root(),
+				AssetId::polkadex, 
+				AssetId::polkadex, 
+				1_u128.into(), 
+				100_u128.into(), 
+				1_u128.into(),
+				100_u128.into(), 
+				100_u128.into(), 
+				10_u128.into(),
+			),
+			Error::<Test>::BothAssetsCannotBeSame
+		);
+	});
+}
+
+#[test]
+fn test_register_trading_pair(){
+	new_test_ext().execute_with(||{
+		assert_ok!(
+			OCEX::register_trading_pair(
+				Origin::root(), 
+				AssetId::asset(10), 
+				AssetId::asset(20), 
+				1_u128.into(),
+				100_u128.into(), 
+				1_u128.into(), 
+				100_u128.into(),
+				100_u128.into(),
+				10_u128.into()
+			)
+		);
+
+		assert_eq!(TradingPairs::<Test>::contains_key(AssetId::asset(10), AssetId::asset(20)), true);
+		assert_eq!(TradingPairsStatus::<Test>::get(AssetId::asset(10), AssetId::asset(20)), true);
+	});
+}
+
+#[test]
+fn test_register_trading_pair_trading_pair_already_registered(){
+	new_test_ext().execute_with(||{
+		assert_ok!(
+			OCEX::register_trading_pair(
+				Origin::root(), 
+				AssetId::asset(10), 
+				AssetId::asset(20), 
+				1_u128.into(),
+				100_u128.into(), 
+				1_u128.into(), 
+				100_u128.into(),
+				100_u128.into(),
+				10_u128.into()
+			)
+		);
+
+		assert_noop!(
+			OCEX::register_trading_pair(
+				Origin::root(), 
+				AssetId::asset(10), 
+				AssetId::asset(20), 
+				1_u128.into(),
+				100_u128.into(), 
+				1_u128.into(), 
+				100_u128.into(),
+				100_u128.into(),
+				10_u128.into()
+			), 
+			Error::<Test>::TradingPairAlreadyRegistered
+		);
+	});
+}
 
 
 fn create_account_id() -> AccountId32{
