29f33603dec59a10a2f82ab7b07ee7960269b876
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-07-27 10:24:09 +0530

    add: single test for registering main account

--- pallets/ocex/src/tests.rs
+++ pallets/ocex/src/tests.rs
@@ -20,6 +20,7 @@ use frame_support::{
 	parameter_types,
 	traits::{ConstU128, ConstU64},
 	PalletId,
+	assert_noop, assert_ok,
 };
 use frame_support::traits::OnTimestampSet;
 use polkadex_primitives::{Moment, Signature};
@@ -31,127 +32,37 @@ use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
+	AccountId32
 };
-// Reexport crate as its pallet name for construct_runtime.
+use crate::mock::*;
+use sp_keystore::{testing::KeyStore, KeystoreExt, SyncCryptoStore};
 
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-type Block = frame_system::mocking::MockBlock<Test>;
+pub const KEY_TYPE: sp_application_crypto::KeyTypeId = sp_application_crypto::KeyTypeId(*b"ocex");
 
-// For testing the pallet, we construct a mock runtime.
-frame_support::construct_runtime!(
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-		Assets: pallet_assets::{Pallet, Call, Storage, Event<T>},
-		Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
-		OCEX: crate::{Pallet, Call, Storage, Event<T>},
-	}
-);
+#[test]
+fn test_register_main_account(){
+	let account_id = create_account_id();
 
-parameter_types! {
-	pub BlockWeights: frame_system::limits::BlockWeights =
-		frame_system::limits::BlockWeights::simple_max(1024);
-}
-impl frame_system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type Origin = Origin;
-	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Call = Call;
-	type Hashing = BlakeTwo256;
-	type AccountId = sp_runtime::AccountId32;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = Event;
-	type BlockHashCount = ConstU64<250>;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = pallet_balances::AccountData<u128>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ();
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+	new_test_ext().execute_with(|| {
+		assert_ok!(OCEX::register_main_account(Origin::signed(account_id.clone().into()), account_id.clone().into()));
+		assert_eq!(Accounts::<Test>::contains_key::<AccountId32>(account_id.into()), true);
+	});
 }
 
-impl pallet_balances::Config for Test {
-	type MaxLocks = ();
-	type MaxReserves = ();
-	type ReserveIdentifier = [u8; 8];
-	type Balance = u128;
-	type DustRemoval = ();
-	type Event = Event;
-	type ExistentialDeposit = ConstU128<1>;
-	type AccountStore = System;
-	type WeightInfo = ();
-}
 
-thread_local! {
-	pub static CAPTURED_MOMENT: RefCell<Option<Moment>> = RefCell::new(None);
-}
 
-pub struct MockOnTimestampSet;
-impl OnTimestampSet<Moment> for MockOnTimestampSet {
-	fn on_timestamp_set(moment: Moment) {
-		CAPTURED_MOMENT.with(|x| *x.borrow_mut() = Some(moment));
-	}
-}
+fn create_account_id() -> AccountId32{
+	const PHRASE: &str =
+		"news slush supreme milk chapter athlete soap sausage put clutch what kitten";
+	let keystore = KeyStore::new();
+	let account_id: AccountId32 = SyncCryptoStore::sr25519_generate_new(
+		&keystore,
+		KEY_TYPE,
+		Some(&format!("{}/hunter1", PHRASE)),
+	)
+	.expect("Unable to create sr25519 key pair")
+	.try_into()
+	.expect("Unable to convert to AccountId32");
 
-impl pallet_timestamp::Config for Test {
-	type Moment = Moment;
-	type OnTimestampSet = MockOnTimestampSet;
-	type MinimumPeriod = ConstU64<5>;
-	type WeightInfo = ();
-}
-
-parameter_types! {
-	pub const ProxyLimit: u32 = 2;
-	pub const OcexPalletId: PalletId = PalletId(*b"OCEX_LMP");
-	pub const MsPerDay: u64 = 86_400_000;
-}
-
-impl Config for Test {
-	type Event = Event;
-	type PalletId = OcexPalletId;
-	type NativeCurrency = Balances;
-	type OtherAssets = Assets;
-	type EnclaveOrigin = EnsureRoot<sp_runtime::AccountId32>;
-	type WeightInfo = ();
-	type Public = <Signature as sp_runtime::traits::Verify>::Signer;
-	type Signature = Signature;
-	type MsPerDay = MsPerDay;
-}
-
-parameter_types! {
-	pub const AssetDeposit: u128 = 100;
-	pub const ApprovalDeposit: u128 = 1;
-	pub const StringLimit: u32 = 50;
-	pub const MetadataDepositBase: u128 = 10;
-	pub const MetadataDepositPerByte: u128 = 1;
-}
-
-impl pallet_assets::Config for Test {
-	type Event = Event;
-	type Balance = u128;
-	type AssetId = u128;
-	type Currency = Balances;
-	type ForceOrigin = EnsureRoot<sp_runtime::AccountId32>;
-	type AssetDeposit = AssetDeposit;
-	type AssetAccountDeposit = AssetDeposit;
-	type MetadataDepositBase = MetadataDepositBase;
-	type MetadataDepositPerByte = MetadataDepositPerByte;
-	type ApprovalDeposit = ApprovalDeposit;
-	type StringLimit = StringLimit;
-	type Freezer = ();
-	type Extra = ();
-	type WeightInfo = ();
+	return account_id;
 }
