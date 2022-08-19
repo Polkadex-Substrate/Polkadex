4f5761f525e6ccf892b89cd54308f0d4fc53f924
zktony <86763036+zktony@users.noreply.github.com>
2022-08-19 19:59:29 +0530

    Added orderbook council (#487)
    * Added orderbook council
    
    * Added new paramenters

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -298,7 +298,7 @@ impl InstanceFilter<Call> for ProxyType {
 				c,
 				Call::Council(..) |
 					Call::TechnicalCommittee(..) |
-					Call::Elections(..) | Call::Treasury(..)
+					Call::Elections(..) | Call::Treasury(..) | Call::OrderbookCommittee(..)
 			),
 			ProxyType::Staking => matches!(c, Call::Staking(..)),
 		}
@@ -782,9 +782,32 @@ impl pallet_collective::Config<TechnicalCollective> for Runtime {
 	type WeightInfo = pallet_collective::weights::SubstrateWeight<Runtime>;
 }
 
+parameter_types! {
+	pub const OrderbookMotionDuration: BlockNumber = 1 * DAYS;
+	pub const OrderbookMaxProposals: u32 = 100;
+	pub const OrderbookMaxMembers: u32 = 3;
+}
+
+type OrderbookCollective = pallet_collective::Instance3;
+impl pallet_collective::Config<OrderbookCollective> for Runtime {
+	type Origin = Origin;
+	type Proposal = Call;
+	type Event = Event;
+	type MotionDuration = OrderbookMotionDuration;
+	type MaxProposals = OrderbookMaxProposals;
+	type MaxMembers = OrderbookMaxMembers;
+	type DefaultVote = pallet_collective::PrimeDefaultVote;
+	type WeightInfo = pallet_collective::weights::SubstrateWeight<Runtime>;
+}
+
 type EnsureRootOrHalfCouncil = EnsureOneOf<
 	EnsureRoot<AccountId>,
 	pallet_collective::EnsureProportionMoreThan<AccountId, CouncilCollective, 1, 2>,
+>;
+
+type EnsureRootOrHalfOrderbookCouncil = EnsureOneOf<
+	EnsureRoot<AccountId>,
+	pallet_collective::EnsureProportionMoreThan<AccountId, OrderbookCollective, 1, 2>,
 >;
 
 impl pallet_membership::Config<pallet_membership::Instance1> for Runtime {
@@ -1256,7 +1279,7 @@ impl pallet_ocex_lmp::Config for Runtime {
 	// TODO: Change origin to SGX attested origin
 	type EnclaveOrigin = EnsureSigned<AccountId>;
 	type Public = <Signature as traits::Verify>::Signer;
-	type GovernanceOrigin = EnsureRootOrHalfCouncil;
+	type GovernanceOrigin = EnsureRootOrHalfOrderbookCouncil;
 	type Signature = Signature;
 	type WeightInfo = ();
 	type MsPerDay = MsPerDay;
@@ -1305,7 +1328,8 @@ construct_runtime!(
 		Assets: pallet_assets::{Pallet, Call, Storage, Event<T>} = 34,
 		PolkadexIdo: polkadex_ido::{Pallet, Call, Event<T>, Storage} = 35,
 		OCEX: pallet_ocex_lmp::{Pallet, Call, Storage, Event<T>} = 36,
-    Token: test_token_provider::{Pallet, Call, Event<T>, ValidateUnsigned} = 37,
+        Token: test_token_provider::{Pallet, Call, Event<T>, ValidateUnsigned} = 37,
+		OrderbookCommittee: pallet_collective::<Instance3>::{Pallet, Call, Storage, Origin<T>, Event<T>} = 38,
 	}
 );
 /// Digest item type.
