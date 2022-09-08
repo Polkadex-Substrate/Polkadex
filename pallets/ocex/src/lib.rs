3bb1fd95703f1f6295f59426621460cb11f36ce3
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-09-08 14:42:55 +0530

    fix: withdrawal bug of double spending

--- pallets/ocex/src/lib.rs
+++ pallets/ocex/src/lib.rs
@@ -71,7 +71,7 @@ pub mod pallet {
 		assets::AssetId,
 		ocex::{AccountInfo, TradingPairConfig},
 		snapshot::{EnclaveSnapshot, Fees},
-		withdrawal::Withdrawal,
+		withdrawal::{Withdrawal, self},
 		AssetsLimit, ProxyLimit, SnapshotAccLimit, WithdrawalLimit, UNIT_BALANCE,
 	};
 	use rust_decimal::{prelude::ToPrimitive, Decimal};
@@ -556,46 +556,56 @@ pub mod pallet {
 		pub fn withdraw(origin: OriginFor<T>, snapshot_id: u32) -> DispatchResult {
 			// Anyone can claim the withdrawal for any user
 			// This is to build services that can enable free withdrawals similar to CEXes.
+			// TODO! Need to add account_id parameter and add an ensure to check if the account_id exists or not
 			let sender = ensure_signed(origin)?;
-
-			let mut withdrawals: WithdrawalsMap<T> = <Withdrawals<T>>::get(snapshot_id);
-			ensure!(withdrawals.contains_key(&sender), Error::<T>::InvalidWithdrawalIndex);
-			if let Some(withdrawal_vector) = withdrawals.get(&sender) {
-				for x in withdrawal_vector.iter() {
-					// TODO: Security: if this fails for a withdrawal in between the iteration, it
-					// will double spend.
-					if let Some(converted_withdrawal) =
-						x.amount.saturating_mul(Decimal::from(UNIT_BALANCE)).to_u128()
-					{
-						Self::transfer_asset(
-							&Self::get_custodian_account(),
-							&x.main_account,
-							converted_withdrawal.saturated_into(),
-							x.asset,
-						)?;
+			// This vector will keep track of withdrawals processed already
+			let mut processed_withdrawals: BoundedVec<Withdrawal<T::AccountId>, WithdrawalLimit> = BoundedVec::<Withdrawal<T::AccountId>, WithdrawalLimit>::default();
+			ensure!(<Withdrawals<T>>::contains_key(&snapshot_id), Error::<T>::InvalidWithdrawalIndex);
+			// This entire block of code is put inside ensure as some of the nested functions will return Err 
+			ensure!(<Withdrawals<T>>::mutate(snapshot_id, |btree_map|{
+				// Get mutable reference to the withdrawals vector
+				if let Some(withdrawal_vector) = btree_map.get_mut(&sender){
+					while withdrawal_vector.len() > 0 {
+						// Perform pop operation to ensure we do not leave any withdrawal left for a double spend
+						if let Some(withdrawal) = withdrawal_vector.pop(){
+							if let Some(converted_withdrawal) = withdrawal.amount.saturating_mul(Decimal::from(UNIT_BALANCE)).to_u128(){
+								if Self::transfer_asset(
+									&Self::get_custodian_account(),
+									&withdrawal.main_account,
+									converted_withdrawal.saturated_into(),
+									withdrawal.asset,
+								).is_ok(){
+									processed_withdrawals.try_push(withdrawal).unwrap_or_default();
+								}
+							} 
+						}
 					}
+					btree_map.remove(&sender);
+					return Ok(())
+				} else {
+					// This allows us to ensure we do not have someone with an invalid account
+					return Err(Error::<T>::InvalidWithdrawalIndex)
 				}
-				Self::deposit_event(Event::WithdrawalClaimed {
-					main: sender.clone(),
-					withdrawals: withdrawal_vector.to_owned(),
-				});
-				ensure!(
-					<OnChainEvents<T>>::mutate(|onchain_events| {
-						onchain_events.try_push(
-							polkadex_primitives::ocex::OnChainEvents::OrderBookWithdrawalClaimed(
-								snapshot_id,
-								sender.clone(),
-								withdrawal_vector.to_owned(),
-							),
-						)?;
-						Ok::<(), ()>(())
-					})
-					.is_ok(),
-					Error::<T>::OnchainEventsBoundedVecOverflow
-				);
-			}
-			withdrawals.remove(&sender);
-			<Withdrawals<T>>::insert(snapshot_id, withdrawals);
+			}).is_ok(), Error::<T>::InvalidWithdrawalIndex);
+			Self::deposit_event(Event::WithdrawalClaimed {
+				main: sender.clone(),
+				withdrawals: processed_withdrawals.clone(),
+			});
+			ensure!(
+				<OnChainEvents<T>>::mutate(|onchain_events| {
+					onchain_events.try_push(
+						polkadex_primitives::ocex::OnChainEvents::OrderBookWithdrawalClaimed(
+							snapshot_id,
+							sender.clone(),
+							processed_withdrawals.clone(),
+						),
+					)?;
+					Ok::<(), ()>(())
+				})
+				.is_ok(),
+				Error::<T>::OnchainEventsBoundedVecOverflow
+			);
+
 			Ok(())
 		}
 
