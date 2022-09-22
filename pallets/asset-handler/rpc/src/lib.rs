1bfb5d7d5855a6cb76048b15157eea3beb936aa5
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-09-22 13:52:18 +0530

    fix: RPC account_balances

--- pallets/asset-handler/rpc/src/lib.rs
+++ pallets/asset-handler/rpc/src/lib.rs
@@ -18,10 +18,10 @@ pub trait PolkadexAssetHandlerRpcApi<BlockHash, AccountId, Hash> {
 	#[method(name = "assethandler_accountbalances")]
 	fn account_balances(
 		&self,
-		assets: Vec<u128>,
+		assets: Vec<String>,
 		account_id: AccountId,
 		at: Option<BlockHash>,
-	) -> RpcResult<Vec<u128>>;
+	) -> RpcResult<Vec<String>>;
 }
 
 pub struct PolkadexAssetHandlerRpc<Client, Block> {
@@ -48,16 +48,22 @@ where
 {
 	fn account_balances(
 		&self,
-		assets: Vec<u128>,
+		assets: Vec<String>,
 		account_id: AccountId,
 		at: Option<<Block as BlockT>::Hash>,
-	) -> RpcResult<Vec<u128>> {
+	) -> RpcResult<Vec<String>> {
+		let assets: RpcResult<Vec<_>> = assets
+			.iter()
+			.map(|asset_id| asset_id.parse::<u128>().map_err(runtime_error_into_rpc_err))
+			.collect();
 		let api = self.client.runtime_api();
 		let at = BlockId::hash(at.unwrap_or_else(||
-            // If the block hash is not supplied assume the best block.
-            self.client.info().best_hash));
+		// If the block hash is not supplied assume the best block.
+		self.client.info().best_hash));
 
-		api.account_balances(&at, assets, account_id)
+		let runtime_api_result = api.account_balances(&at, assets?, account_id);
+		runtime_api_result
+			.map(|balances| balances.iter().map(|balance| balance.to_string()).collect())
 			.map_err(runtime_error_into_rpc_err)
 	}
 }
