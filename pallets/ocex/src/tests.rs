8bcd2753ff8899afb5da9eed899247e9d75f9055
nakul1010 <nakul.amate@gmail.com>
2022-09-27 18:26:58 +0530

    added the comments backed

--- pallets/ocex/src/tests.rs
+++ pallets/ocex/src/tests.rs
@@ -1,4 +1,20 @@
-use crate::*;
+// This file is part of Polkadex.
+
+// Copyright (C) 2020-2022 Polkadex oü.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+//! Tests for pallet-ocex.use crate::*;
+
 use frame_support::{assert_noop, assert_ok, bounded_vec, traits::OnInitialize};
 use polkadex_primitives::{
 	assets::AssetId, ingress::IngressMessages, withdrawal::Withdrawal, Moment, SnapshotAccLimit,
