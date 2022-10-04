c7931dab51079f2e9c1922e9c8302170ea636d93
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-10-04 10:15:02 +0530

    fix: withdrawal double spend bug

--- pallets/ocex/src/lib.rs
+++ pallets/ocex/src/lib.rs
@@ -725,6 +725,7 @@ pub mod pallet {
 		/// Withdraws user balance
 		///
 		/// params: snapshot_number: u32
+		/// account: AccountId
 		#[pallet::weight((100000 as Weight).saturating_add(T::DbWeight::get().reads(2 as Weight)).saturating_add(T::DbWeight::get().writes(3 as Weight)))]
 		pub fn claim_withdraw(
 			origin: OriginFor<T>,
@@ -734,45 +735,54 @@ pub mod pallet {
 			// Anyone can claim the withdrawal for any user
 			// This is to build services that can enable free withdrawals similar to CEXes.
 			let _ = ensure_signed(origin)?;
-
-			let mut withdrawals: WithdrawalsMap<T> = <Withdrawals<T>>::get(snapshot_id);
-			ensure!(withdrawals.contains_key(&account), Error::<T>::InvalidWithdrawalIndex);
-			if let Some(withdrawal_vector) = withdrawals.get(&account) {
-				for x in withdrawal_vector.iter() {
-					// TODO: Security: if this fails for a withdrawal in between the iteration, it
-					// will double spend.
-					if let Some(converted_withdrawal) =
-						x.amount.saturating_mul(Decimal::from(UNIT_BALANCE)).to_u128()
-					{
-						Self::transfer_asset(
-							&Self::get_custodian_account(),
-							&x.main_account,
-							converted_withdrawal.saturated_into(),
-							x.asset,
-						)?;
+			// This vector will keep track of withdrawals processed already
+			let mut processed_withdrawals: BoundedVec<Withdrawal<T::AccountId>, WithdrawalLimit> = BoundedVec::<Withdrawal<T::AccountId>, WithdrawalLimit>::default();
+			ensure!(<Withdrawals<T>>::contains_key(&snapshot_id), Error::<T>::InvalidWithdrawalIndex);
+			// This entire block of code is put inside ensure as some of the nested functions will return Err
+			ensure!(<Withdrawals<T>>::mutate(snapshot_id, |btree_map|{
+				// Get mutable reference to the withdrawals vector
+				if let Some(withdrawal_vector) = btree_map.get_mut(&account){
+					while withdrawal_vector.len() > 0 {
+						// Perform pop operation to ensure we do not leave any withdrawal left for a double spend
+						if let Some(withdrawal) = withdrawal_vector.pop(){
+							if let Some(converted_withdrawal) = withdrawal.amount.saturating_mul(Decimal::from(UNIT_BALANCE)).to_u128(){
+								if Self::transfer_asset(
+									&Self::get_custodian_account(),
+									&withdrawal.main_account,
+									converted_withdrawal.saturated_into(),
+									withdrawal.asset,
+								).is_ok(){
+									processed_withdrawals.try_push(withdrawal).unwrap_or_default();
+								}
+							}
+						}
 					}
+					btree_map.remove(&account);
+					return Ok(())
+				} else {
+					// This allows us to ensure we do not have someone with an invalid account
+					return Err(Error::<T>::InvalidWithdrawalIndex)
 				}
-				Self::deposit_event(Event::WithdrawalClaimed {
-					main: account.clone(),
-					withdrawals: withdrawal_vector.to_owned(),
-				});
-				ensure!(
-					<OnChainEvents<T>>::mutate(|onchain_events| {
-						onchain_events.try_push(
-							polkadex_primitives::ocex::OnChainEvents::OrderBookWithdrawalClaimed(
-								snapshot_id,
-								account.clone(),
-								withdrawal_vector.to_owned(),
-							),
-						)?;
-						Ok::<(), ()>(())
-					})
-					.is_ok(),
-					Error::<T>::OnchainEventsBoundedVecOverflow
-				);
-			}
-			withdrawals.remove(&account);
-			<Withdrawals<T>>::insert(snapshot_id, withdrawals);
+			}).is_ok(), Error::<T>::InvalidWithdrawalIndex);
+			Self::deposit_event(Event::WithdrawalClaimed {
+				main: account.clone(),
+				withdrawals: processed_withdrawals.clone(),
+			});
+			ensure!(
+				<OnChainEvents<T>>::mutate(|onchain_events| {
+					onchain_events.try_push(
+						polkadex_primitives::ocex::OnChainEvents::OrderBookWithdrawalClaimed(
+							snapshot_id,
+							account.clone(),
+							processed_withdrawals.clone(),
+						),
+					)?;
+					Ok::<(), ()>(())
+				})
+				.is_ok(),
+				Error::<T>::OnchainEventsBoundedVecOverflow
+			);
+
 			Ok(())
 		}
 
