ca674a8d207331aceea74d785fad9aa9401e7129
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-10-04 10:21:45 +0530

    refactor: rustfmt fixes

--- pallets/ocex/src/lib.rs
+++ pallets/ocex/src/lib.rs
@@ -736,34 +736,52 @@ pub mod pallet {
 			// This is to build services that can enable free withdrawals similar to CEXes.
 			let _ = ensure_signed(origin)?;
 			// This vector will keep track of withdrawals processed already
-			let mut processed_withdrawals: BoundedVec<Withdrawal<T::AccountId>, WithdrawalLimit> = BoundedVec::<Withdrawal<T::AccountId>, WithdrawalLimit>::default();
-			ensure!(<Withdrawals<T>>::contains_key(&snapshot_id), Error::<T>::InvalidWithdrawalIndex);
-			// This entire block of code is put inside ensure as some of the nested functions will return Err
-			ensure!(<Withdrawals<T>>::mutate(snapshot_id, |btree_map|{
-				// Get mutable reference to the withdrawals vector
-				if let Some(withdrawal_vector) = btree_map.get_mut(&account){
-					while withdrawal_vector.len() > 0 {
-						// Perform pop operation to ensure we do not leave any withdrawal left for a double spend
-						if let Some(withdrawal) = withdrawal_vector.pop(){
-							if let Some(converted_withdrawal) = withdrawal.amount.saturating_mul(Decimal::from(UNIT_BALANCE)).to_u128(){
-								if Self::transfer_asset(
-									&Self::get_custodian_account(),
-									&withdrawal.main_account,
-									converted_withdrawal.saturated_into(),
-									withdrawal.asset,
-								).is_ok(){
-									processed_withdrawals.try_push(withdrawal).unwrap_or_default();
+			let mut processed_withdrawals: BoundedVec<Withdrawal<T::AccountId>, WithdrawalLimit> =
+				BoundedVec::<Withdrawal<T::AccountId>, WithdrawalLimit>::default();
+			ensure!(
+				<Withdrawals<T>>::contains_key(&snapshot_id),
+				Error::<T>::InvalidWithdrawalIndex
+			);
+			// This entire block of code is put inside ensure as some of the nested functions will
+			// return Err
+			ensure!(
+				<Withdrawals<T>>::mutate(snapshot_id, |btree_map| {
+					// Get mutable reference to the withdrawals vector
+					if let Some(withdrawal_vector) = btree_map.get_mut(&account) {
+						while withdrawal_vector.len() > 0 {
+							// Perform pop operation to ensure we do not leave any withdrawal left
+							// for a double spend
+							if let Some(withdrawal) = withdrawal_vector.pop() {
+								if let Some(converted_withdrawal) = withdrawal
+									.amount
+									.saturating_mul(Decimal::from(UNIT_BALANCE))
+									.to_u128()
+								{
+									if Self::transfer_asset(
+										&Self::get_custodian_account(),
+										&withdrawal.main_account,
+										converted_withdrawal.saturated_into(),
+										withdrawal.asset,
+									)
+									.is_ok()
+									{
+										processed_withdrawals
+											.try_push(withdrawal)
+											.unwrap_or_default();
+									}
 								}
 							}
 						}
+						btree_map.remove(&account);
+						return Ok(())
+					} else {
+						// This allows us to ensure we do not have someone with an invalid account
+						return Err(Error::<T>::InvalidWithdrawalIndex)
 					}
-					btree_map.remove(&account);
-					return Ok(())
-				} else {
-					// This allows us to ensure we do not have someone with an invalid account
-					return Err(Error::<T>::InvalidWithdrawalIndex)
-				}
-			}).is_ok(), Error::<T>::InvalidWithdrawalIndex);
+				})
+				.is_ok(),
+				Error::<T>::InvalidWithdrawalIndex
+			);
 			Self::deposit_event(Event::WithdrawalClaimed {
 				main: account.clone(),
 				withdrawals: processed_withdrawals.clone(),
