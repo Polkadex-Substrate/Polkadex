154b7c5a4224f65832c238d05f12374723917c0d
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2023-01-04 14:44:28 +0530

    feat: add primitives for controller messages and connector events

---
+++ relayer/src/primitives.rs
@@ -0,0 +1,13 @@
+// Controller Messages from Controller to Connectors
+// Controller -> Native Connector
+// Controller -> Foreign Connector
+pub enum ControllerActions {}
+
+// Native Connector Messages from Connector to Controller
+// Native Connector -> Controller
+pub enum NativeConnectorEvents {}
+
+// Foreign Connector Messages from Connector to Controller
+// Foreign Connector -> Controller
+pub enum ForeignConnectorEvents {}
+
