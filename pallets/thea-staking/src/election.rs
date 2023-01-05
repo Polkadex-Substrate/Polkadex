b5505a6e8f61e5600ed05233782a64d480ef161d
Gautham <gauthamastro@gmail.com>
2023-01-05 18:06:49 +0530

    Remove nomination

--- pallets/thea-staking/src/election.rs
+++ pallets/thea-staking/src/election.rs
@@ -1,10 +1,10 @@
-use polkadex_primitives::AccountId;
+
 use crate::{BalanceOf, Config};
 use crate::pallet::Stakinglimits;
 use crate::session::Exposure;
 
 /// Algorithm to elect relayers from all candidates
-pub fn elect_relayers<T: Config>(mut candidates: Vec<(AccountId, Exposure<AccountId,BalanceOf<T>>)>) -> Vec<(AccountId, Exposure<AccountId,BalanceOf<T>>)>{
+pub fn elect_relayers<T: Config>(mut candidates: Vec<(T::AccountId, Exposure<T::AccountId,BalanceOf<T>>)>) -> Vec<(T::AccountId, Exposure<T::AccountId,BalanceOf<T>>)>{
     // If we don't have preffered number of relayers we take everyone
     let max_relayers = <Stakinglimits<T>>::get().max_relayers;
     if candidates.len() <= max_relayers as usize {
