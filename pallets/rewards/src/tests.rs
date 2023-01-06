b1e5efe9c9fab1912efbb5d5ac145500ecc0d53a
nakul1010 <nakul.amate@gmail.com>
2023-01-06 08:28:22 +0530

    rewards pallet two extrinsic + test cases added

--- pallets/rewards/src/tests.rs
+++ pallets/rewards/src/tests.rs
@@ -1,91 +1,167 @@
-use crate as simple_event;
-use frame_support::{assert_ok, parameter_types};
-use sp_core::H256;
+use crate::*;
+use frame_support::{assert_noop, assert_ok};
+// The testing primitives are very useful for avoiding having to work with signatures
+// or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
+use crate::mock::*;
+use frame_system::EventRecord;
+use polkadex_primitives::AccountId;
+use sp_runtime::{AccountId32, BoundedVec, DispatchError::BadOrigin};
+use std::convert::TryFrom;
 
-use sp_io::TestExternalities;
-
-use sp_runtime::{
-	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup},
-};
-
-use frame_system as system;
-use frame_system::{EventRecord, Phase};
-
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-type Block = frame_system::mocking::MockBlock<Test>;
-
-// Configure a mock runtime to test the pallet.
-frame_support::construct_runtime!(
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Module, Call, Config, Storage, Event<T>},
-		SimpleEvent: simple_event::{Module, Call, Storage, Event<T>},
-	}
-);
-
-parameter_types! {
-	pub const BlockHashCount: u64 = 250;
-	pub const SS58Prefix: u8 = 42;
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+	let events = frame_system::Pallet::<T>::events();
+	let system_event: <T as frame_system::Config>::Event = generic_event.into();
+	// compare to the last event record
+	let EventRecord { event, .. } = &events[events.len() - 1];
+	assert_eq!(event, &system_event);
 }
 
-impl system::Config for Test {
-	type BaseCallFilter = ();
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type Origin = Origin;
-	type Call = Call;
-	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = Event;
-	type BlockHashCount = BlockHashCount;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = SS58Prefix;
+//Bob main account id
+pub const BOB_ACCOUNT_RAW_ID: [u8; 32] = [6u8; 32];
+//Bob proxy account id
+pub const ALICE_ACCOUNT_RAW_ID: [u8; 32] = [7u8; 32];
+
+fn get_alice_account_with_rewards() -> (AccountId32, u128) {
+	(AccountId::new(ALICE_ACCOUNT_RAW_ID), 100)
 }
 
-impl simple_event::Config for Test {
-	type Event = Event;
+fn get_bob_account_with_rewards() -> (AccountId32, u128) {
+	(AccountId::new(BOB_ACCOUNT_RAW_ID), 200)
 }
 
-struct ExternalityBuilder;
-
-impl ExternalityBuilder {
-	pub fn build() -> TestExternalities {
-		let storage = frame_system::GenesisConfig::default()
-			.build_storage::<Test>()
-			.unwrap();
-		let mut ext = TestExternalities::from(storage);
-		ext.execute_with(|| System::set_block_number(1));
-		ext
-	}
+fn get_parameters_for_reward_cycle() -> (u32, u32, u32, u32) {
+	(2, 5, 10, 1)
 }
 
 #[test]
-fn test() {
-	ExternalityBuilder::build().execute_with(|| {
-		assert_ok!(SimpleEvent::do_something(Origin::signed(1), 32));
-
-		assert_eq!(
-			System::events(),
-			vec![EventRecord {
-				phase: Phase::Initialization,
-				event: Event::simple_event(simple_event::Event::EmitInput(32)),
-				topics: vec![],
-			}]
+fn create_reward_cycle() {
+	new_test_ext().execute_with(|| {
+		let (start_block, end_block, intial_percentage, id) = get_parameters_for_reward_cycle();
+		assert_ok!(Rewards::create_reward_cycle(
+			Origin::root(),
+			start_block,
+			end_block,
+			intial_percentage,
+			id
+		));
+		assert_last_event::<Test>(
+			crate::Event::RewardCycleCreated { start_block, end_block, id }.into(),
 		);
-	})
+		let reward_info = IntializeRewards::<Test>::get(&id).unwrap();
+		assert_eq!(reward_info.start_block, start_block);
+		assert_eq!(reward_info.end_block, end_block);
+		assert_eq!(reward_info.intial_percentage, intial_percentage);
+	});
+}
+
+#[test]
+fn create_reward_cycle_with_invalid_root() {
+	new_test_ext().execute_with(|| {
+		let (start_block, end_block, intial_percentage, id) = get_parameters_for_reward_cycle();
+		assert_noop!(
+			Rewards::create_reward_cycle(
+				Origin::none(),
+				start_block,
+				end_block,
+				intial_percentage,
+				id
+			),
+			BadOrigin
+		);
+		assert_eq!(IntializeRewards::<Test>::get(&id), None)
+	});
+}
+
+#[test]
+fn create_reward_cycle_for_existing_id() {
+	new_test_ext().execute_with(|| {
+		let (start_block, end_block, intial_percentage, id) = get_parameters_for_reward_cycle();
+		assert_ok!(Rewards::create_reward_cycle(
+			Origin::root(),
+			start_block,
+			end_block,
+			intial_percentage,
+			id
+		));
+		assert_noop!(
+			Rewards::create_reward_cycle(
+				Origin::root(),
+				start_block,
+				end_block,
+				intial_percentage,
+				id
+			),
+			Error::<Test>::DuplicateId
+		);
+	});
+}
+
+#[test]
+fn create_reward_cycle_when_start_block_greater_than_end_block() {
+	new_test_ext().execute_with(|| {
+		let (start_block, end_block, intial_percentage, id) = get_parameters_for_reward_cycle();
+		assert_noop!(
+			Rewards::create_reward_cycle(
+				Origin::root(),
+				end_block,
+				start_block,
+				intial_percentage,
+				id
+			),
+			Error::<Test>::InvalidParameter
+		);
+	});
+}
+
+#[test]
+fn add_reward_beneficiaries_with_invalid_root() {
+	new_test_ext().execute_with(|| {
+		let (_, _, _, id) = get_parameters_for_reward_cycle();
+		let vec_of_ids: Vec<(AccountId32, u128)> = vec![];
+		assert_noop!(
+			Rewards::add_reward_beneficiaries(
+				Origin::none(),
+				id,
+				BoundedVec::try_from(vec_of_ids).unwrap()
+			),
+			BadOrigin
+		);
+	});
+}
+
+#[test]
+fn add_reward_beneficiaries_when_reward_id_not_register() {
+	new_test_ext().execute_with(|| {
+		let (_, _, _, id) = get_parameters_for_reward_cycle();
+		let vec_of_ids: Vec<(AccountId32, u128)> = vec![];
+		assert_noop!(
+			Rewards::add_reward_beneficiaries(
+				Origin::root(),
+				id,
+				BoundedVec::try_from(vec_of_ids).unwrap()
+			),
+			Error::<Test>::RewardIdNotRegister
+		);
+	});
+}
+
+#[test]
+fn add_reward_beneficiaries() {
+	new_test_ext().execute_with(|| {
+		let (start_block, end_block, intial_percentage, id) = get_parameters_for_reward_cycle();
+		assert_ok!(Rewards::create_reward_cycle(
+			Origin::root(),
+			start_block,
+			end_block,
+			intial_percentage,
+			id
+		));
+		let vec_of_ids: Vec<(AccountId32, u128)> =
+			vec![get_alice_account_with_rewards(), get_bob_account_with_rewards()];
+		assert_ok!(Rewards::add_reward_beneficiaries(
+			Origin::root(),
+			id,
+			BoundedVec::try_from(vec_of_ids).unwrap()
+		));
+	});
 }
