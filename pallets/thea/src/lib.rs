727d65e055e3d64994bf10f7c961fe7669f8ced9
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2023-01-09 14:14:14 +0530

    fix: apply rustfmt

--- pallets/thea/src/lib.rs
+++ pallets/thea/src/lib.rs
@@ -45,9 +45,9 @@ pub mod pallet {
 	use thea_primitives::BLSPublicKey;
 
 	#[derive(Encode, Decode, Clone, Debug, MaxEncodedLen, TypeInfo)]
-	pub struct ApprovedDeposit{
+	pub struct ApprovedDeposit {
 		pub asset_id: u128,
-		pub amount: u128
+		pub amount: u128,
 	}
 
 	#[derive(Encode, Decode, Clone, MaxEncodedLen, TypeInfo, PartialEq, Debug)]
@@ -57,7 +57,7 @@ pub mod pallet {
 		pub tx_hash: sp_core::H256,
 		pub asset_id: u128,
 		pub amount: u128,
-		pub deposit_nonce: u32
+		pub deposit_nonce: u32,
 	}
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
@@ -108,14 +108,7 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn get_deposit_nonce)]
-	pub(super) type DepositNonce<T: Config> = StorageMap<
-		_,
-		Blake2_128Concat,
-		u8,
-		u32,
-		ValueQuery,
-	>;
-
+	pub(super) type DepositNonce<T: Config> = StorageMap<_, Blake2_128Concat, u8, u32, ValueQuery>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://substrate.dev/docs/en/knowledgebase/runtime/events
@@ -123,7 +116,7 @@ pub mod pallet {
 	#[pallet::generate_deposit(pub (super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		// Deposit Approved event ( recipient, asset_id, amount, tx_hash(foreign chain))
-		DepositApproved(T::AccountId, u128, u128, sp_core::H256)
+		DepositApproved(T::AccountId, u128, u128, sp_core::H256),
 	}
 
 	// Errors inform users that something went wrong.
@@ -136,7 +129,7 @@ pub mod pallet {
 		// Asset has not been registered
 		AssetNotRegistered,
 		// BLS Aggregate signature failed
-		BLSSignatureVerificationFailed
+		BLSSignatureVerificationFailed,
 	}
 
 	// Hooks for Thea Pallet are defined here
@@ -146,7 +139,6 @@ pub mod pallet {
 	// Extrinsics for Thea Pallet are defined here
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-
 		/// A Deposit transaction is called by the nodes with an aggregated BLS Signature
 		///
 		/// # Parameters
@@ -158,17 +150,25 @@ pub mod pallet {
 		/// * `tx_hash`: Hash of the transaction on the foreign chain
 		/// * `asset_id`: The asset id of the asset being deposited
 		/// * `amount`: The amount of assets that have been deposited in foreign chain
-		/// * `bls_signature`: The aggregated signature of majority of relayers in current active relayer set
+		/// * `bls_signature`: The aggregated signature of majority of relayers in current active
+		///   relayer set
 		#[pallet::weight(1000)]
-		pub fn approve_deposit(origin: OriginFor<T>, bit_map: u128, bls_signature: [u8;96], payload: Payload<T::AccountId>) -> DispatchResult {
+		pub fn approve_deposit(
+			origin: OriginFor<T>,
+			bit_map: u128,
+			bls_signature: [u8; 96],
+			payload: Payload<T::AccountId>,
+		) -> DispatchResult {
 			ensure!(payload.amount > 0, Error::<T>::AmountCannotBeZero);
 			// Fetch Deposit Nonce
 			let nonce = <DepositNonce<T>>::get(payload.network_id);
-			ensure!(payload.deposit_nonce == nonce+1, Error::<T>::DepositNonceError);
-			// ensure!(asset_handler::pallet::TheaAssets::<T>::contains_key(payload.asset_id), Error::<T>::AssetNotRegistered);
+			ensure!(payload.deposit_nonce == nonce + 1, Error::<T>::DepositNonceError);
+			// ensure!(asset_handler::pallet::TheaAssets::<T>::contains_key(payload.asset_id),
+			// Error::<T>::AssetNotRegistered);
 
 			// Fetch current active relayer set BLS Keys
-			let current_active_relayer_set = Self::get_relayers_key_vector(payload.network_id).unwrap();
+			let current_active_relayer_set =
+				Self::get_relayers_key_vector(payload.network_id).unwrap();
 
 			// Call host function with current_active_relayer_set, signature, bit_map, verify nonce
 			// TODO: @gautham
@@ -176,32 +176,47 @@ pub mod pallet {
 			// Step 1: Get Payload, Signature, BLS Keys Vector
 			// Step 2: Create Aggregate BLS Public Key
 			// Step 3: Verify Aggregate Signature
-			ensure!(thea_primitives::thea_ext::foo(bls_signature, bit_map, payload.encode(), current_active_relayer_set.into_inner()), Error::<T>::BLSSignatureVerificationFailed);
+			ensure!(
+				thea_primitives::thea_ext::foo(
+					bls_signature,
+					bit_map,
+					payload.encode(),
+					current_active_relayer_set.into_inner()
+				),
+				Error::<T>::BLSSignatureVerificationFailed
+			);
 
 			// Update deposit Nonce
-			<DepositNonce<T>>::insert(payload.network_id, nonce+1);
+			<DepositNonce<T>>::insert(payload.network_id, nonce + 1);
 
 			// Update Storage item
-			let approved_deposit = ApprovedDeposit{asset_id: payload.asset_id, amount: payload.amount};
+			let approved_deposit =
+				ApprovedDeposit { asset_id: payload.asset_id, amount: payload.amount };
 			if <ApprovedDeposits<T>>::contains_key(&payload.who) {
-				<ApprovedDeposits<T>>::mutate(payload.who.clone(), |bounded_vec|{
-					if let Some(inner_bounded_vec) = bounded_vec{
+				<ApprovedDeposits<T>>::mutate(payload.who.clone(), |bounded_vec| {
+					if let Some(inner_bounded_vec) = bounded_vec {
 						inner_bounded_vec.try_push(approved_deposit).unwrap();
 					}
 				});
 			} else {
 				let mut my_vec: BoundedVec<ApprovedDeposit, ConstU32<100>> = Default::default();
 				if let Ok(()) = my_vec.try_push(approved_deposit) {
-					<ApprovedDeposits<T>>::insert::<T::AccountId, BoundedVec<ApprovedDeposit, ConstU32<100>>>(payload.who.clone(), my_vec);
+					<ApprovedDeposits<T>>::insert::<
+						T::AccountId,
+						BoundedVec<ApprovedDeposit, ConstU32<100>>,
+					>(payload.who.clone(), my_vec);
 				}
 			}
 
 			// Emit event
-			Self::deposit_event(Event::<T>::DepositApproved(payload.who, payload.asset_id, payload.amount, payload.tx_hash));
+			Self::deposit_event(Event::<T>::DepositApproved(
+				payload.who,
+				payload.asset_id,
+				payload.amount,
+				payload.tx_hash,
+			));
 			Ok(())
 		}
-
-
 	}
 
 	// Helper Functions for Thea Pallet
--- thea-primitives/src/lib.rs
+++ thea-primitives/src/lib.rs
@@ -1,88 +1,91 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use sp_runtime_interface::runtime_interface;
 #[cfg(feature = "std")]
 use blst::min_sig::*;
 #[cfg(feature = "std")]
 use blst::BLST_ERROR;
+use sp_runtime_interface::runtime_interface;
 
+use sp_std::{vec, vec::Vec};
 
-use sp_std::vec::{Vec};
-use sp_std::vec;
-
-use parity_scale_codec::{Encode, Decode, MaxEncodedLen};
+use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
 use scale_info::TypeInfo;
 
 #[runtime_interface]
 pub trait TheaExt {
-    fn foo(agg_sig: [u8; 96], bit_map: u128, payload: Vec<u8>, bls_public_keys: Vec<BLSPublicKey>) -> bool {
-        let recon_sig = Signature::from_bytes(&agg_sig).unwrap();
-        let bit_map_vec = return_set_bits(bit_map);
-        let mut agg_pk: Option<AggregatePublicKey> = None;
-        for x in bit_map_vec{
-            // Fetch public key
-            let current_public_key = &bls_public_keys[x as usize];
-            // Create public key from Vec from bytes
-            let bls_key = PublicKey::from_bytes(&bls_public_keys[x as usize].0).unwrap();
-            // Add Public key to the already aggregated public key
-            if agg_pk.is_none() {
-                agg_pk = match AggregatePublicKey::aggregate(&[&bls_key], false) {
-                    Ok(agg_pk) => Some(agg_pk),
-                    Err(err) => {return false},
-                };
-            } else {
-                let mut new_agg_pk = agg_pk.unwrap();
-                new_agg_pk.add_public_key(&bls_key, false).unwrap();
-                agg_pk = Some(new_agg_pk);
-            }
-        }
-        // Generate Aggregate Signature
-        let mut agg_sig = AggregateSignature::from_signature(&recon_sig);
-        let dst = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
-        let err = recon_sig.fast_aggregate_verify_pre_aggregated(
-            false,
-            &payload,
-            dst,
-            &agg_pk.unwrap().to_public_key(),
-        );
-        if err == BLST_ERROR::BLST_SUCCESS {
-            return true;
-        }
-        false
-    }
+	fn foo(
+		agg_sig: [u8; 96],
+		bit_map: u128,
+		payload: Vec<u8>,
+		bls_public_keys: Vec<BLSPublicKey>,
+	) -> bool {
+		let recon_sig = Signature::from_bytes(&agg_sig).unwrap();
+		let bit_map_vec = return_set_bits(bit_map);
+		let mut agg_pk: Option<AggregatePublicKey> = None;
+		for x in bit_map_vec {
+			// Fetch public key
+			let current_public_key = &bls_public_keys[x as usize];
+			// Create public key from Vec from bytes
+			let bls_key = PublicKey::from_bytes(&bls_public_keys[x as usize].0).unwrap();
+			// Add Public key to the already aggregated public key
+			if agg_pk.is_none() {
+				agg_pk = match AggregatePublicKey::aggregate(&[&bls_key], false) {
+					Ok(agg_pk) => Some(agg_pk),
+					Err(err) => return false,
+				};
+			} else {
+				let mut new_agg_pk = agg_pk.unwrap();
+				new_agg_pk.add_public_key(&bls_key, false).unwrap();
+				agg_pk = Some(new_agg_pk);
+			}
+		}
+		// Generate Aggregate Signature
+		let mut agg_sig = AggregateSignature::from_signature(&recon_sig);
+		let dst = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
+		let err = recon_sig.fast_aggregate_verify_pre_aggregated(
+			false,
+			&payload,
+			dst,
+			&agg_pk.unwrap().to_public_key(),
+		);
+		if err == BLST_ERROR::BLST_SUCCESS {
+			return true
+		}
+		false
+	}
 }
 
 #[derive(Debug, Encode, Decode, PartialEq, TypeInfo, MaxEncodedLen)]
 pub struct BLSPublicKey(pub [u8; 192]);
 
-pub fn return_set_bits(bit_map: u128) -> Vec<u8>{
-    let mut set_bits: Vec<u8> = vec![];
-    for i in 0..128 {
-        if (bit_map & 2_u128.pow(i as u32)) == 2_u128.pow(i as u32){
-            set_bits.push(i as u8);
-        }
-    }
-    set_bits
+pub fn return_set_bits(bit_map: u128) -> Vec<u8> {
+	let mut set_bits: Vec<u8> = vec![];
+	for i in 0..128 {
+		if (bit_map & 2_u128.pow(i as u32)) == 2_u128.pow(i as u32) {
+			set_bits.push(i as u8);
+		}
+	}
+	set_bits
 }
 
 #[test]
-pub fn test_bit_manipulation(){
-    let x = 3;
-    let mut set_bits: Vec<u8> = vec![];
-    let mut i: usize = 0;
-    while i < 128 {
-        if (x & 2_u128.pow(i as u32)) == 2_u128.pow(i as u32){
-            set_bits.push(i as u8);
-        }
-        i += 1;
-    }
-    assert_eq!(set_bits, vec![0,1]);
+pub fn test_bit_manipulation() {
+	let x = 3;
+	let mut set_bits: Vec<u8> = vec![];
+	let mut i: usize = 0;
+	while i < 128 {
+		if (x & 2_u128.pow(i as u32)) == 2_u128.pow(i as u32) {
+			set_bits.push(i as u8);
+		}
+		i += 1;
+	}
+	assert_eq!(set_bits, vec![0, 1]);
 }
 
 #[test]
-pub fn test_set_bit_map(){
-    let x: u128 = 2;
-    // Set 0th bit
-    let new_x = (1 << 0) | x;
-    assert_eq!(new_x, 3);
+pub fn test_set_bit_map() {
+	let x: u128 = 2;
+	// Set 0th bit
+	let new_x = (1 << 0) | x;
+	assert_eq!(new_x, 3);
 }
