a00835660f544c369cd4d3e0d18e3ddb045ed687
zktony <zktony@protonmail.com>
2023-01-23 11:20:13 +0530

    Fixed deposit

--- thea-primitives/src/parachain_primitives.rs
+++ thea-primitives/src/parachain_primitives.rs
@@ -10,6 +10,19 @@ use xcm::{
 	latest::{Fungibility, MultiAsset, MultiLocation},
 	prelude::Xcm,
 };
+use xcm::latest::AssetId;
+
+#[derive(Encode, Decode, Clone, TypeInfo, PartialEq, Debug)]
+pub enum AssetType {
+	Fungible,
+	NonFungible
+}
+
+#[derive(Encode, Decode, Clone, TypeInfo, PartialEq, Debug)]
+pub struct ParachainAsset {
+	pub location: MultiLocation,
+	pub asset_type: AssetType
+}
 
 #[derive(Encode, Decode, Clone, TypeInfo, PartialEq, Debug)]
 pub struct ParachainDeposit {
@@ -28,23 +41,40 @@ impl ParachainDeposit {
 			_ => None,
 		}
 	}
+
+	pub fn get_parachain_asset(&self) -> Option<ParachainAsset> {
+		let MultiAsset { id, .. } = self.asset_and_amount.clone();
+		if let AssetId::Concrete(multilocation) = id {
+			Some(ParachainAsset {
+				location: multilocation,
+				asset_type: AssetType::Fungible,
+			})
+		} else {
+			None
+		}
+
+	}
 }
 
 impl AssetIdConverter for ParachainDeposit {
 	fn get_asset_id(&self) -> Option<u128> {
-		if let Ok(asset_identifier) =
-			BoundedVec::<u8, ConstU32<100>>::try_from(Encode::encode(self))
-		{
-			let identifier_length = asset_identifier.len();
-			let mut derived_asset_id: Vec<u8> = vec![];
-			derived_asset_id.push(self.network_id);
-			derived_asset_id.push(identifier_length as u8);
-			derived_asset_id.extend(&asset_identifier.to_vec());
-			let derived_asset_id_hash =
-				&sp_io::hashing::keccak_256(derived_asset_id.as_ref())[0..16];
-			let mut temp = [0u8; 16];
-			temp.copy_from_slice(derived_asset_id_hash);
-			Some(u128::from_le_bytes(temp))
+		if let Some(parachain_asset) = self.get_parachain_asset() {
+			if let Ok(asset_identifier) =
+			BoundedVec::<u8, ConstU32<1000>>::try_from(parachain_asset.encode())
+			{
+				let identifier_length = asset_identifier.len();
+				let mut derived_asset_id: Vec<u8> = vec![];
+				derived_asset_id.push(self.network_id);
+				derived_asset_id.push(identifier_length as u8);
+				derived_asset_id.extend(&asset_identifier.to_vec());
+				let derived_asset_id_hash =
+					&sp_io::hashing::keccak_256(derived_asset_id.as_ref())[0..16];
+				let mut temp = [0u8; 16];
+				temp.copy_from_slice(derived_asset_id_hash);
+				Some(u128::from_le_bytes(temp))
+			} else {
+				None
+			}
 		} else {
 			None
 		}
