48f2c852a6cd5ec87827ca2f7908b159ff3cb344
nakul1010 <nakul.amate@gmail.com>
2023-02-28 11:27:43 +0530

    added fmt

--- pallets/assets-transaction-payment/src/payment.rs
+++ pallets/assets-transaction-payment/src/payment.rs
@@ -143,11 +143,8 @@ where
 		if !matches!(can_withdraw, WithdrawConsequence::Success) {
 			return Err(InvalidTransaction::Payment.into())
 		}
-		<T::Fungibles as Balanced<T::AccountId>>::withdraw(asset_id, who, converted_fee).map_err(
-			|_err| {
-				TransactionValidityError::from(InvalidTransaction::Payment)
-			},
-		)
+		<T::Fungibles as Balanced<T::AccountId>>::withdraw(asset_id, who, converted_fee)
+			.map_err(|_err| TransactionValidityError::from(InvalidTransaction::Payment))
 	}
 
 	/// Hand the fee and the tip over to the `[HandleCredit]` implementation.
