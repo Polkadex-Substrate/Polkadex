e8f0a4a629cce0368afadae50b6ca3aebdec206c
nakul1010 <nakul.amate@gmail.com>
2023-02-28 14:56:47 +0530

    added flags in pallet

--- pallets/ocex/src/lib.rs
+++ pallets/ocex/src/lib.rs
@@ -15,6 +15,7 @@
 
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
+#![deny(unused_crate_dependencies)]
 
 use frame_support::{
 	dispatch::DispatchResult,
