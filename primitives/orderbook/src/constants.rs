7c1e4919ad4c172abb52bc08ae94cb8e6e5387ff
Gautham <gauthamastro@gmail.com>
2023-03-16 18:41:29 +0530

    Moving stuff

---
+++ primitives/orderbook/src/constants.rs
@@ -0,0 +1,18 @@
+use polkadex_primitives::Balance;
+
+pub const GRANDPA_AUTHORITIES_KEY: &[u8] = b":grandpa_authorities";
+pub const POLKADEX_MAINNET_SS58: u16 = 88;
+
+pub const UNIT_BALANCE: Balance = 1_000_000_000_000_u128;
+// Range of QTY: 0.00000001 to 10,000,000 UNITs
+pub const MIN_QTY: Balance = UNIT_BALANCE / 10000000;
+pub const MAX_QTY: Balance = 10000000 * UNIT_BALANCE;
+// Range of PRICE: 0.00000001 to 10,000,000 UNITs
+pub const MIN_PRICE: Balance = UNIT_BALANCE / 10000000;
+pub const MAX_PRICE: Balance = 10000000 * UNIT_BALANCE;
+pub const ADDRESSFORMAT: u8 = 88u8;
+
+#[test]
+pub fn test_overflow_check() {
+    assert!(MAX_PRICE.checked_mul(MAX_QTY).is_some());
+}
