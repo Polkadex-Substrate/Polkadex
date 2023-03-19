0d6d77cd9060f7bac7cda19cda67d41e038bf8ce
Gautham <gauthamastro@gmail.com>
2023-03-19 20:47:49 +0530

    Add max withdrawals per snapshot

--- primitives/orderbook/src/constants.rs
+++ primitives/orderbook/src/constants.rs
@@ -3,6 +3,7 @@ use polkadex_primitives::Balance;
 pub const GRANDPA_AUTHORITIES_KEY: &[u8] = b":grandpa_authorities";
 pub const POLKADEX_MAINNET_SS58: u16 = 88;
 
+pub const MAX_WITHDRAWALS_PER_SNAPSHOT: u8 = 20;
 pub const UNIT_BALANCE: Balance = 1_000_000_000_000_u128;
 // Range of QTY: 0.00000001 to 10,000,000 UNITs
 pub const MIN_QTY: Balance = UNIT_BALANCE / 10000000;
