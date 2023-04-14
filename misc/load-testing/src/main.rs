20151e605bc8e44252bbd85e3834f482b4a5afed
Gautham <gauthamastro@gmail.com>
2023-04-14 17:00:19 +0530

    Fix stid bug.

--- misc/load-testing/src/main.rs
+++ misc/load-testing/src/main.rs
@@ -1,6 +1,7 @@
 use jsonrpsee::{core::client::ClientT, rpc_params, ws_client::WsClientBuilder};
-use orderbook_primitives::types::{ObMessage, UserActions};
-use sp_core::Pair;
+use orderbook_primitives::types::{ObMessage, UserActions, ObRecoveryState, WithdrawalRequest, WithdrawPayloadCallByUser};
+use sp_core::{Pair, Encode};
+use polkadex_primitives::{AccountId,Signature, AssetId};
 
 #[tokio::main]
 async fn main() {
@@ -14,14 +15,32 @@ async fn main() {
 			123, 228, 3, 109, 30, 110, 21]);
 	// Public key is 0x02a7d451190f72881cd92a044127adf6417b788e5118f4934484fe4d789860eb33
 	println!("public: {:?}", pair.public());
+	let main = sp_keyring::AccountKeyring::Alice;
+	let proxy = sp_keyring::AccountKeyring::Bob;
+
+	let payload = WithdrawPayloadCallByUser {
+		asset_id: AssetId::Polkadex,
+		amount: "1".to_string(),
+		timestamp: 0,
+	};
 
 	let mut message = ObMessage {
-		stid: 2,
-		action: UserActions::BlockImport(25),
+		stid: 8,
+		action: UserActions::Withdraw(WithdrawalRequest{
+			signature: Signature::from(proxy.sign(&payload.encode())),
+			payload,
+			main: AccountId::from(main.public()),
+			proxy: AccountId::from(proxy.public()),
+		}),
 		signature: Default::default()
 	};
 
 	message.signature = pair.sign_prehashed(&message.sign_data());
 
 	client.request("ob_submitAction", rpc_params![message]).await.unwrap()
+
+	// let result: Vec<u8> = client.request("ob_getObRecoverState", rpc_params![]).await.unwrap();
+	//
+	// let result: ObRecoveryState = serde_json::from_slice(&result).unwrap();
+	// println!("{:?}",result);
 }
