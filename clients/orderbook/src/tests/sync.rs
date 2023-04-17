0983d84fed82411cdedb3fd17f4b1cbfda1eaabb
Gautham <gauthamastro@gmail.com>
2023-04-17 15:42:09 +0530

    Basic super majority snapshotting works.

--- clients/orderbook/src/tests/sync.rs
+++ clients/orderbook/src/tests/sync.rs
@@ -1,14 +1,16 @@
 use crate::tests::{
 	generate_and_finalize_blocks, initialize_orderbook, make_ob_ids, ObTestnet, TestApi,
 };
-use orderbook_primitives::crypto::AuthorityId;
+use futures::SinkExt;
+use orderbook_primitives::{
+	crypto::AuthorityId,
+	types::{ObMessage, UserActions},
+};
 use sc_network_test::TestNetFactory;
 use sp_consensus::BlockOrigin;
 use sp_core::Pair;
 use sp_keyring::AccountKeyring;
 use std::{sync::Arc, time::Duration};
-use futures::SinkExt;
-use orderbook_primitives::types::{ObMessage, UserActions};
 
 #[tokio::test]
 pub async fn test_orderbook_sync() {
@@ -24,6 +26,8 @@ pub async fn test_orderbook_sync() {
 
 	let active: Vec<AuthorityId> =
 		make_ob_ids(&peers.iter().map(|(k, _)| k.clone()).collect::<Vec<AccountKeyring>>());
+
+	println!("Acive list: {:?}", active);
 
 	let validator_api = Arc::new(TestApi {
 		active,
@@ -45,24 +49,27 @@ pub async fn test_orderbook_sync() {
 
 	let future = initialize_orderbook(&mut testnet, ob_peers).await;
 	tokio::spawn(future);
-	// Generate and finalize one block
+	// Generate and finalize two block to start finality
 	generate_and_finalize_blocks(1, &mut testnet).await;
-	// Generate and finalize one block
 	generate_and_finalize_blocks(1, &mut testnet).await;
 	// Send the RPC with Ob message
-	let mut message = ObMessage{
-		stid: 1,
-		action: UserActions::BlockImport(1),
-		signature: Default::default(),
-	};
-	message.signature = orderbook_operator.sign(&message.sign_data());
-	testnet.peers[1].data.peer_rpc_link.as_ref().unwrap().send(message).await.unwrap();
+	let mut message =
+		ObMessage { stid: 1, action: UserActions::BlockImport(1), signature: Default::default() };
+	message.signature = orderbook_operator.sign_prehashed(&message.sign_data());
+	testnet.peers[0]
+		.data
+		.peer_rpc_link
+		.as_ref()
+		.unwrap()
+		.send(message)
+		.await
+		.unwrap();
 	testnet.run_until_sync().await;
 
 	// Generate and finalize one block
 	generate_and_finalize_blocks(5, &mut testnet).await;
 
 	testnet.run_until_idle().await;
-
-	tokio::time::sleep(Duration::from_secs(100)).await;
+	// We should have generated one snapshot by this point
+	assert_eq!(validator_api.snapshots.read().len(), 1);
 }
