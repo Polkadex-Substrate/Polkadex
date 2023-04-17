b45fb637a4e3f608ccfb26d8aa5adb1ed335b743
Gautham <gauthamastro@gmail.com>
2023-04-17 11:17:45 +0530

    Testing framework is ported

--- clients/orderbook/src/tests/sync.rs
+++ clients/orderbook/src/tests/sync.rs
@@ -1,3 +1,39 @@
+use crate::tests::{initialize_orderbook, make_ob_ids, ObTestnet, TestApi};
+use orderbook_primitives::crypto::AuthorityId;
+use sp_core::Pair;
+use sp_keyring::AccountKeyring;
+use std::sync::Arc;
 
+#[tokio::test]
+pub async fn test_orderbook_sync() {
+	let (orderbook_operator, _) = sp_core::ecdsa::Pair::generate();
+	let mut testnet = ObTestnet::new(3, 0);
+	let peers = &[
+		(AccountKeyring::Alice, true),
+		(AccountKeyring::Bob, true),
+		(AccountKeyring::Charlie, true),
+	];
 
+	let active: Vec<AuthorityId> =
+		make_ob_ids(&peers.iter().map(|(k, _)| k.clone()).collect::<Vec<AccountKeyring>>());
 
+	let validator_api = Arc::new(TestApi {
+		active,
+		latest_snapshot_nonce: Arc::new(Default::default()),
+		snapshots: Arc::new(Default::default()),
+		unprocessed: Arc::new(Default::default()),
+		main_to_proxy_mapping: Default::default(),
+		pending_snapshot: None,
+		operator_key: Some(orderbook_operator.public()),
+		trading_config: vec![],
+		withdrawals: Arc::new(Default::default()),
+	});
+
+	let ob_peers = peers
+		.iter()
+		.enumerate()
+		.map(|(id, (key, is_auth))| (id, key, validator_api.clone(), *is_auth))
+		.collect();
+
+	initialize_orderbook(&mut testnet, ob_peers).await;
+}
