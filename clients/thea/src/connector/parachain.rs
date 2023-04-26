71d2b6cd4f47616a462a1000713b36e272dba3cc
Gautham <gauthamastro@gmail.com>
2023-04-26 08:41:12 +0530

    Fix subxt

--- clients/thea/src/connector/parachain.rs
+++ clients/thea/src/connector/parachain.rs
@@ -1,18 +1,19 @@
 use std::time::Duration;
 
 use async_trait::async_trait;
-use parity_scale_codec::Encode;
-use subxt::{dynamic::Value, storage::DynamicAddress, OnlineClient, PolkadotConfig};
+use parity_scale_codec::{Decode, Encode};
+use sp_arithmetic::traits::Saturating;
+use subxt::{dynamic::Value, storage::DynamicAddress, subxt, OnlineClient, PolkadotConfig};
 use thea_primitives::types::Message;
 
 use crate::{connector::traits::ForeignConnector, error::Error, types::GossipMessage};
 
+#[subxt::subxt(runtime_metadata_path = "../parachain-metadata/metadata.scale")]
+mod parachain {}
+
 pub struct ParachainClient {
 	api: OnlineClient<PolkadotConfig>,
 }
-
-// This should not be changed unless pallet name in parachain-runtime/lib is changed.
-const PALLET_NAME: &str = "TheaMessageHandler";
 
 #[async_trait]
 impl ForeignConnector for ParachainClient {
@@ -28,96 +29,56 @@ impl ForeignConnector for ParachainClient {
 
 	async fn read_events(&self, last_processed_nonce: u64) -> Result<Option<Message>, Error> {
 		// Read thea messages from foreign chain
-		let storage_address = subxt::dynamic::storage(
-			PALLET_NAME,
-			"OutgoingMessages",
-			vec![
-				// Something that encodes to an AccountId32 is what we need for the map key here:
-				Value::from_bytes(last_processed_nonce.saturating_add(1).encode()),
-			],
-		);
+		let storage_address = parachain::storage()
+			.thea_message_handler()
+			.outgoing_messages(last_processed_nonce.saturating_add(1));
 		// TODO: Get last finalized block hash
-		let encoded_bytes = self
-			.api
-			.storage()
-			.at_latest()
-			.await?
-			.fetch_or_default(&storage_address)
-			.await?
-			.into_encoded();
+		let encoded_bytes =
+			self.api.storage().at_latest().await?.fetch(&storage_address).await?.encode();
 
 		Ok(parity_scale_codec::Decode::decode(&mut &encoded_bytes[..])?)
 	}
 
-	async fn send_transaction(&self, message: GossipMessage) {
-		let call = subxt::dynamic::tx(
-			PALLET_NAME,
-			"incoming_message",
-			vec![
-				// Bitmap
-				Value::from(message.bitmap.clone()),
-				// Payload
-				Value::from_bytes(message.payload.encode()),
-				// Signature
-				Value::from_bytes(message.aggregate_signature.encode()),
-			],
+	async fn send_transaction(&self, message: GossipMessage) -> Result<(), Error> {
+		let call = parachain::tx().thea_message_handler().incoming_message(
+			message.bitmap,
+			Decode::decode(&mut &message.payload.encode()[..])?,
+			Decode::decode(&mut &message.aggregate_signature.encode()[..])?,
 		);
 
 		self.api
 			.tx()
-			.create_unsigned(&call)
-			.unwrap()
+			.create_unsigned(&call)?
 			.submit_and_watch()
-			.await
-			.unwrap()
+			.await?
 			.wait_for_in_block()
-			.await
-			.unwrap();
+			.await?;
+		Ok(())
 	}
 
 	async fn check_message(&self, message: &Message) -> Result<bool, Error> {
 		// Read thea messages from foreign chain
-		let storage_address = subxt::dynamic::storage(
-			PALLET_NAME,
-			"OutgoingMessages",
-			vec![
-				// Something that encodes to an AccountId32 is what we need for the map key here:
-				Value::from_bytes(message.nonce.encode()),
-			],
-		);
+		let storage_address =
+			parachain::storage().thea_message_handler().outgoing_messages(message.nonce);
 		// TODO: Get last finalized block hash
-		let encoded_bytes = self
-			.api
-			.storage()
-			.at_latest()
-			.await?
-			.fetch_or_default(&storage_address)
-			.await?
-			.into_encoded();
+		let encoded_bytes =
+			self.api.storage().at_latest().await?.fetch(&storage_address).await?.encode();
 
 		let message_option: Option<Message> =
 			parity_scale_codec::Decode::decode(&mut &encoded_bytes[..])?;
-		let message_from_chain = message_option.ok_or(Error::ErrorReadingTheaMessage)?;
 
-		Ok(message_from_chain == message.clone())
+		match message_option {
+			None => return Ok(false),
+			Some(message_from_chain) => Ok(message_from_chain == message.clone()),
+		}
 	}
 
 	async fn last_processed_nonce_from_native(&self) -> Result<u64, Error> {
 		// Read native network nonce from foreign chain
-		let storage_address: DynamicAddress<Value> =
-			subxt::dynamic::storage(PALLET_NAME, "IncomingNonce", vec![]);
+		let storage_address = parachain::storage().thea_message_handler().incoming_nonce();
 		// TODO: Get last finalized block hash
-		let encoded_bytes = self
-			.api
-			.storage()
-			.at_latest()
-			.await?
-			.fetch_or_default(&storage_address)
-			.await?
-			.into_encoded();
-
-		let message_option: u64 = parity_scale_codec::Decode::decode(&mut &encoded_bytes[..])?;
-
-		Ok(message_option)
+		let nonce =
+			self.api.storage().at_latest().await?.fetch_or_default(&storage_address).await?;
+		Ok(nonce)
 	}
 }
