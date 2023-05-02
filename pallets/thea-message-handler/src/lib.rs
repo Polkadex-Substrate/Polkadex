538d82374ec79f178dccaecf89d047d3d0411337
zktony <zktony@protonmail.com>
2023-05-02 09:32:58 +0530

    Updated vec dependency

--- pallets/thea-message-handler/src/lib.rs
+++ pallets/thea-message-handler/src/lib.rs
@@ -34,7 +34,7 @@ use thea_primitives::{types::Message, Network, ValidatorSet};
 pub mod pallet {
 	use super::*;
 	use frame_support::transactional;
-	use sp_std::vec;
+	use sp_std::vec::Vec;
 	use thea_primitives::{types::Message, TheaIncomingExecutor};
 
 	#[pallet::config]
