6978ec7040ec54b5e499c9e4377a26727980b277
Gautham <gauthamastro@gmail.com>
2023-05-03 17:49:01 +0530

    Added logs

--- clients/orderbook/src/snapshot.rs
+++ clients/orderbook/src/snapshot.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 /// This is a dummy struct used to serialize memory db
 /// We cannot serialize the hashmap below because of non-string type in key.
 #[serde_as]
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct SnapshotStore {
 	#[serde_as(as = "JsonString<Vec<(JsonString, _)>>")]
 	pub map: HashMap<[u8; 32], (Vec<u8>, i32)>,
