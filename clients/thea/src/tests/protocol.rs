0eab8a3815121c560460baa2faa7a563a1f641da
35359595 <35359595i@gmail.com>
2023-05-11 11:28:30 +0200

    initial set id change test

--- clients/thea/src/tests/protocol.rs
+++ clients/thea/src/tests/protocol.rs
@@ -8,7 +8,7 @@ use std::collections::HashMap;
 use substrate_test_runtime_client::Ed25519Keyring;
 
 #[tokio::test]
-async fn dropped_one_validator_still_works() {
+async fn dropped_one_validator_still_works_test() {
 	sp_tracing::try_init_simple();
 
 	let network = 1;
@@ -127,6 +127,84 @@ async fn dropped_one_validator_still_works() {
 		!foreign_connector.incoming_messages.read().is_empty(),
 		"No signature submitted to foreign chain"
 	);
+
+	// terminate
+	thea_handle.abort_handle().abort();
+	grandpa_handle.abort_handle().abort();
+}
+
+#[tokio::test]
+async fn validator_set_change_mid_messaging_works_test() {
+	sp_tracing::try_init_simple();
+
+	let network = 1;
+	let peers = &[
+		(AccountKeyring::Alice, true),
+		(AccountKeyring::Bob, true),
+		(AccountKeyring::Charlie, true),
+	];
+
+	let active: Vec<AuthorityId> =
+		make_thea_ids(&peers.iter().map(|(k, _)| k.clone()).collect::<Vec<AccountKeyring>>());
+
+	let message = Message {
+		block_no: 10,
+		nonce: 1,
+		data: vec![1, 2, 3],
+		network,
+		is_key_change: false,
+		validator_set_id: 0,
+		validator_set_len: 3,
+	};
+	let grandpa_peers = &[Ed25519Keyring::Alice, Ed25519Keyring::Bob, Ed25519Keyring::Charlie];
+	let genesys_authorities = make_gradpa_ids(grandpa_peers);
+
+	let runtime = Arc::new(TestApi {
+		genesys_authorities,
+		authorities: BTreeMap::from([(
+			network,
+			ValidatorSet { set_id: 0, validators: active.clone() },
+		)]),
+		validator_set_id: 0,
+		_next_authorities: BTreeMap::new(),
+		network_pref: BTreeMap::from([
+			(active[0].clone(), network),
+			(active[1].clone(), network),
+			(active[2].clone(), network),
+		]),
+		outgoing_messages: BTreeMap::from([((network, 1), message.clone())]),
+		incoming_messages: Arc::new(RwLock::new(BTreeMap::new())),
+		incoming_nonce: Arc::new(RwLock::new(BTreeMap::new())),
+		_outgoing_nonce: BTreeMap::from([(network, 1)]),
+	});
+
+	let foreign_connector = Arc::new(DummyForeignConnector {
+		authorities: HashMap::from([(0, active)]),
+		incoming_nonce: Arc::new(RwLock::new(0)),
+		incoming_messages: Arc::new(RwLock::new(HashMap::new())),
+	});
+
+	let mut testnet = TheaTestnet::new(3, 0, runtime.clone());
+
+	let validators = peers
+		.iter()
+		.enumerate()
+		.map(|(id, (key, is_auth))| (id, key, runtime.clone(), *is_auth, foreign_connector.clone()))
+		.collect();
+
+	let grandpa_handle = tokio::spawn(initialize_grandpa(&mut testnet, grandpa_peers));
+	let networking = testnet.peer(0).network_service().clone();
+	let thea_handle = tokio::spawn(initialize_thea(&mut testnet, validators).await);
+
+	// add new block
+	testnet.peer(0).push_blocks(1, false);
+	testnet.run_until_sync().await;
+
+	// rotate id
+
+	// send message with old id
+
+	// verify still works
 
 	// terminate
 	thea_handle.abort_handle().abort();
