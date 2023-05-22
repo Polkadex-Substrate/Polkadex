1b1e8e411526ddd89f67297ecdac8d1c22b7c86d
Gautham <Gauthamastro@gmail.com>
2023-05-22 18:30:30 +0530

    Update test.rs

--- primitives/thea/src/test.rs
+++ primitives/thea/src/test.rs
@@ -1,4 +1,3 @@
-use crate::types::Withdraw;
 use sp_core::Pair;
 use sp_runtime::traits::IdentifyAccount;
 
@@ -12,17 +11,4 @@ fn get_thea_public_key() {
 	)
 	.unwrap();
 	println!("public_key : {:?}", hex::encode(thea.public().into_account()));
-}
-
-use parity_scale_codec::Decode;
-
-#[test]
-fn decode_withdraw() {
-	let data = "04807a9d37eb4c4026ad79e3513d598c316720cda462eb5739954a52247291404f5388d9c8207fa69f3812744433c6417f3a00008a5d7845630100000000000000007001010200a10f0300838559281acd8ba8ebce4c4bbe8d6aeb85cc03f20000";
-
-	let bytes = hex::decode(data).unwrap();
-
-	let payload: Vec<Withdraw> = Decode::decode(&mut &bytes[..]).unwrap();
-
-	println!("payload: {:?}", payload);
 }
