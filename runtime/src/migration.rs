2b7641c1de0c498ac43635d21ad828cf470fe40f
Gautham <gauthamastro@gmail.com>
2023-06-12 16:43:13 +0530

    Implement OnRuntimeUpgrade trait

--- runtime/src/migration.rs
+++ runtime/src/migration.rs
@@ -1,9 +1,13 @@
 pub mod session_keys {
 	use frame_support::{pallet_prelude::Weight, traits::StorageVersion};
+	use frame_support::traits::OnRuntimeUpgrade;
+	use log::log;
 	use sp_runtime::impl_opaque_keys;
 	use sp_std::vec::Vec;
-	use crate::{Runtime, Grandpa, Babe, ImOnline, AuthorityDiscovery, SessionKeys};
+
 	use polkadex_primitives::AccountId;
+
+	use crate::{AuthorityDiscovery, Babe, Grandpa, ImOnline, Runtime, SessionKeys};
 
 	impl_opaque_keys! {
 		pub struct SessionKeysV4 {
@@ -46,9 +50,39 @@ pub mod session_keys {
 				}
 				Some(new_keys)
 			}) {
-				log::error!(target:"migration","Storage type cannot be intepreted as the Vec<(AccountId, SessionKeysV4)>")
+				log::error!(target:"migration","Storage type cannot be interpreted as the Vec<(AccountId, SessionKeysV4)>")
 			}
 			Weight::zero()
+		}
+	}
+
+	impl OnRuntimeUpgrade for MigrateToV2 {
+		fn on_runtime_upgrade() -> Weight {
+			let current = pallet_session::<Runtime>::current_storage_version();
+			let onchain = pallet_session::<Runtime>::on_chain_storage_version();
+
+			log!(
+				info,
+				"Running migration with current storage version {:?} / onchain {:?}",
+				current,
+				onchain
+			);
+
+			Self::run(current)
+		}
+
+		#[cfg(feature = "try-runtime")]
+		fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
+			Ok(Vec::new())
+		}
+
+		#[cfg(feature = "try-runtime")]
+		fn post_upgrade(_: Vec<u8>) -> Result<(), &'static str> {
+			let session_keys: Vec<(AccountId, SessionKeys)> = pallet_session::QueuedKeys::<Runtime>::get();
+			if session_keys.is_empty() {
+				return Err("Error reading sessiong keys after upgrade")
+			}
+			Ok(())
 		}
 	}
 }
