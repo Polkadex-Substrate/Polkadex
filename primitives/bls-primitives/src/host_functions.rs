b447f2cf26012ce1b00fed377de395b9dd4d3e02
Gautham <gauthamastro@gmail.com>
2023-06-13 15:50:36 +0530

    Fixed BLS

--- primitives/bls-primitives/src/host_functions.rs
+++ primitives/bls-primitives/src/host_functions.rs
@@ -3,7 +3,7 @@ use sp_core::crypto::KeyTypeId;
 #[cfg(feature = "std")]
 use sp_core::Pair;
 #[cfg(feature = "std")]
-use sp_keystore::{KeystoreExt, SyncCryptoStore};
+use sp_keystore::{KeystoreExt, Keystore};
 use sp_std::vec::Vec;
 
 use crate::Public;
@@ -29,7 +29,7 @@ pub trait BLSCryptoExt {
 			.extension::<KeystoreExt>()
 			.expect("No `keystore` associated for the current context!");
 		let public_key = pair.public().to_raw_vec();
-		SyncCryptoStore::insert_unknown(keystore, id, seed.as_str(), public_key.as_slice())
+		<(dyn Keystore + 'static)>::insert(keystore, id, seed.as_str(), public_key.as_slice())
 			.unwrap();
 		pair.public()
 	}
