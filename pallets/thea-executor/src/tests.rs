20f4916b2271169a60a09e14f0281239de3aa8f9
gautham <gauthamastro@gmail.com>
2023-06-14 12:08:09 +0530

    Fix test

--- pallets/thea-executor/src/tests.rs
+++ pallets/thea-executor/src/tests.rs
@@ -68,7 +68,7 @@ fn test_transfer_native_asset() {
 		// Set withdrawal Fee
 		assert_ok!(TheaExecutor::set_withdrawal_fee(RuntimeOrigin::root(), 1, 0));
 		assert_ok!(TheaExecutor::withdraw(
-			RuntimeOrigin::signed(1),
+			RuntimeOrigin::signed(user),
 			asset_id.clone(),
 			10_000_000_000_000u128,
 			vec![1; 32],
