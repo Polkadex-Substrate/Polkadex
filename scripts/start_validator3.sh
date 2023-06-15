64b7a84b1dd24ac206a0bdcf0a98cef70b0b7f29
Gautham <gauthamastro@gmail.com>
2023-06-15 20:50:59 +0530

    update thea gossip topic

--- scripts/start_validator3.sh
+++ scripts/start_validator3.sh
@@ -17,7 +17,7 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 ../target/release/polkadex-node --validator --port 30335 --base-path ./validator03  \
-    --ws-port=9947 --rpc-port=9948 --chain=../scripts/customSpecRaw.json \
+    -lthea=trace -lorderbook=trace --rpc-port=9948 --chain=../scripts/customSpecRaw.json \
     --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWRozCnsH7zCYiNVpCRqgaoxukPdYxqaPQNs9rdDMDeN4t \
     --bootnodes /ip4/127.0.0.1/tcp/30334/p2p/12D3KooWEVBdwVmV1BeAdtqzhjANK31ibYmLQXxEoeai4fx7KhNh \
-    --node-key=24f121a84149f784f9fe3f1e2fb04e8873191a510bc4b073a3a815d78a29cf2d
+    --node-key=24f121a84149f784f9fe3f1e2fb04e8873191a510bc4b073a3a815d78a29cf2d --thea-dummy-mode
