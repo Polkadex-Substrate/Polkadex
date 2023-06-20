0d27db6b7a6127e57e438cf4d17faa7325c6df9a
Ivan Temchenko <13215338+35359595@users.noreply.github.com>
2023-06-20 22:13:19 +0200

    Update pallets/thea/src/tests.rs
    Co-authored-by: serhii-temchenko <107397548+serhii-temchenko@users.noreply.github.com>

--- pallets/thea/src/tests.rs
+++ pallets/thea/src/tests.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Tests for pallet-ocex.
+//! Tests for "thea" pallet.
 
 use crate::{mock::*, *};
 use bls_primitives::Pair;
