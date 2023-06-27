5a91938052a3bd3f8b9162ff57ae7a803d863aff
35359595 <35359595i@gmail.com>
2023-06-27 15:04:34 +0200

    unsignid submission check; event check; encoding checks;

--- pallets/thea-message-handler/src/test.rs
+++ pallets/thea-message-handler/src/test.rs
@@ -17,21 +17,29 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use crate::{
-	fixtures::produce_authorities,
+	fixtures::{produce_authorities, M, M_KC, SIG},
 	mock::{new_test_ext, Test, *},
 	pallet::*,
+	TransactionSource, *,
 };
 use frame_support::{
 	assert_noop, assert_ok,
 	traits::{fungible::Mutate as FungibleMutate, fungibles::Mutate as FungiblesMutate},
 };
 use frame_system::EventRecord;
-use parity_scale_codec::Encode;
+use parity_scale_codec::{Decode, Encode};
 use sp_runtime::{
 	traits::{AccountIdConversion, BadOrigin},
 	SaturatedConversion,
 };
-use thea_primitives::types::{AssetMetadata, Deposit, Withdraw};
+use thea_primitives::{
+	types::{AssetMetadata, Deposit, Withdraw},
+	ValidatorSet,
+};
+
+fn get_valid_signature<T: Config>() -> T::Signature {
+	<T as crate::Config>::Signature::decode(&mut SIG.as_ref()).unwrap()
+}
 
 fn assert_last_event<T: crate::Config>(generic_event: <T as crate::Config>::RuntimeEvent) {
 	let events = frame_system::Pallet::<T>::events();
@@ -62,5 +70,113 @@ fn test_insert_authorities_full() {
 		));
 		assert_eq!(<Authorities<Test>>::get(111), authorities);
 		assert_eq!(<ValidatorSetId<Test>>::get(), 111);
+	})
+}
+
+#[test]
+fn test_incomming_message_full() {
+	new_test_ext().execute_with(|| {
+		// bad origins
+		assert_noop!(
+			TheaHandler::incoming_message(
+				RuntimeOrigin::root(),
+				vec!(u128::MAX),
+				M.clone(),
+				get_valid_signature::<Test>()
+			),
+			BadOrigin
+		);
+		assert_noop!(
+			TheaHandler::incoming_message(
+				RuntimeOrigin::signed(1),
+				vec!(u128::MAX),
+				M.clone(),
+				get_valid_signature::<Test>()
+			),
+			BadOrigin
+		);
+		// root
+		assert_ok!(TheaHandler::incoming_message(
+			RuntimeOrigin::none(),
+			vec!(u128::MAX),
+			M.clone(),
+			get_valid_signature::<Test>()
+		));
+		// bad signature in unsigned verification
+		let mut direct = M.clone();
+		direct.validator_set_len = 100;
+		let bad_signature_call = Call::<Test>::incoming_message {
+			bitmap: vec![u128::MAX],
+			payload: direct,
+			signature: get_valid_signature::<Test>(),
+		};
+		assert!(
+			TheaHandler::validate_unsigned(TransactionSource::Local, &bad_signature_call).is_err()
+		);
+		// bad message in unsigned verification
+		let bad_message_call = Call::<Test>::incoming_message {
+			bitmap: vec![u128::MAX],
+			payload: M.clone(), // proper message
+			signature: <Test as Config>::Signature::decode(&mut [0u8; 48].as_ref()).unwrap(),
+		};
+		assert!(
+			TheaHandler::validate_unsigned(TransactionSource::Local, &bad_message_call).is_err()
+		);
+		// proper unsigned validation
+		let good_call = Call::<Test>::incoming_message {
+			bitmap: vec![u128::MAX],
+			payload: M.clone(), // proper message
+			signature: get_valid_signature::<Test>(),
+		};
+		assert!(TheaHandler::validate_unsigned(TransactionSource::Local, &good_call).is_ok());
+		// bad nonce
+		let mut vs = M_KC.clone();
+		assert_noop!(
+			TheaHandler::incoming_message(
+				RuntimeOrigin::none(),
+				vec!(u128::MAX),
+				vs.clone(),
+				get_valid_signature::<Test>()
+			),
+			Error::<Test>::MessageNonce
+		);
+		vs.nonce += 1;
+		// Error Deconding keychange
+		assert_noop!(
+			TheaHandler::incoming_message(
+				RuntimeOrigin::none(),
+				vec!(u128::MAX),
+				vs.clone(),
+				get_valid_signature::<Test>()
+			),
+			Error::<Test>::ErrorDecodingValidatorSet
+		);
+		// invalid validator set id
+		let validators = ValidatorSet::new(vec![1u64, 2u64, 3u64], 1).unwrap();
+		let encoded = validators.encode();
+		assert_eq!(validators, ValidatorSet::decode(&mut encoded.as_ref()).unwrap());
+		vs.data = encoded.clone();
+		assert_noop!(
+			TheaHandler::incoming_message(
+				RuntimeOrigin::none(),
+				vec!(u128::MAX),
+				vs.clone(),
+				get_valid_signature::<Test>()
+			),
+			Error::<Test>::InvalidValidatorSetId
+		);
+		// proper validator set change
+		<ValidatorSetId<Test>>::set(0);
+		assert_eq!(<ValidatorSetId<Test>>::get(), 0);
+		assert_ok!(TheaHandler::incoming_message(
+			RuntimeOrigin::none(),
+			vec!(u128::MAX),
+			vs.clone(),
+			get_valid_signature::<Test>()
+		));
+		// actually inserted
+		assert_eq!(<Authorities<Test>>::get(1).len(), 1);
+		// event validation
+		assert_last_event::<Test>(Event::<Test>::TheaMessageExecuted { message: vs }.into());
 	})
 }
