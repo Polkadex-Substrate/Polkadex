99a8383941e7d85c41775334ba25f421d7c8cdd1
Gautham <gauthamastro@gmail.com>
2023-07-12 07:40:41 +0300

    Update thea primitives

--- primitives/thea/src/lib.rs
+++ primitives/thea/src/lib.rs
@@ -36,57 +36,8 @@ use sp_application_crypto::ByteArray;
 use sp_runtime::{traits::IdentifyAccount, DispatchResult};
 use sp_std::vec::Vec;
 
-/// Key type for Orderbook module.
-pub const KEY_TYPE: sp_application_crypto::KeyTypeId = sp_application_crypto::KeyTypeId(*b"thea");
-
-/// Orderbook cryptographic types.
-///
-/// This module basically introduces three crypto types:
-/// - `crypto::Pair`
-/// - `crypto::Public`
-/// - `crypto::Signature`
-///
-/// Your code should use the above types as concrete types for all crypto related
-/// functionality.
-///
-/// The current underlying crypto scheme used is BLS. This can be changed,
-/// without affecting code restricted against the above listed crypto types.
-pub mod crypto {
-	use sp_application_crypto::app_crypto;
-
-	use bls_primitives as BLS;
-
-	app_crypto!(BLS, crate::KEY_TYPE);
-
-	/// Identity of a Orderbook authority using BLS as its crypto.
-	pub type AuthorityId = Public;
-
-	/// Signature for a Orderbook authority using BLS as its crypto.
-	pub type AuthoritySignature = Signature;
-}
-
-impl IdentifyAccount for AuthorityId {
-	type AccountId = Self;
-
-	fn into_account(self) -> Self {
-		self
-	}
-}
-
-#[cfg(feature = "std")]
-impl TryFrom<[u8; 96]> for crypto::AuthorityId {
-	type Error = ();
-	fn try_from(value: [u8; 96]) -> Result<Self, Self::Error> {
-		crypto::AuthorityId::from_slice(&value)
-	}
-}
-
 /// Authority set id starts with zero at genesis.
 pub const GENESIS_AUTHORITY_SET_ID: u64 = 0;
-
-/// Thea worker prefix.
-pub const THEA_WORKER_PREFIX: &[u8; 18] = b"Thea Worker Prefix";
-
 /// A typedef for validator set id.
 pub type ValidatorSetId = u64;
 
@@ -141,26 +92,6 @@ pub const NATIVE_NETWORK: Network = 0;
 
 /// TTL of the cached message.
 pub const MESSAGE_CACHE_DURATION_IN_SECS: u64 = 60;
-
-sp_api::decl_runtime_apis! {
-	/// APIs necessary for Thea.
-	pub trait TheaApi
-	{
-		/// Return the current active Thea validator set for all networks.
-		fn full_validator_set() -> Option<ValidatorSet<AuthorityId>>;
-		/// Return the current active Thea validator set.
-		fn validator_set(network: Network) -> Option<ValidatorSet<AuthorityId>>;
-		/// Returns the outgoing message for given network and blk.
-		fn outgoing_messages(network: Network, nonce: u64) -> Option<Message>;
-		/// Get Thea network associated with Validator.
-		fn network(auth: AuthorityId) -> Option<Network>;
-		/// Incoming messages.
-		#[allow(clippy::result_unit_err)]
-		fn incoming_message(message: Message, bitmap: Vec<u128>, signature: AuthoritySignature) -> Result<(),()>;
-		/// Get last processed nonce for a given network.
-		fn get_last_processed_nonce(network: Network) -> u64;
-	}
-}
 
 /// Thea incoming message executor abstraction which should be implemented by the "Thea Executor".
 pub trait TheaIncomingExecutor {
