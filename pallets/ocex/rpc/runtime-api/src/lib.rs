81a68eedef2a36548bfe476e4aa2cac6296603bb
35359595 <35359595i@gmail.com>
2023-07-25 15:08:10 +0200

    initial ocex rpc server and traits declarations

---
+++ pallets/ocex/rpc/runtime-api/src/lib.rs
@@ -0,0 +1,30 @@
+// This file is part of Polkadex.
+//
+// Copyright (c) 2023 Polkadex oü.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use orderbook_primitives::recovery::ObRecoveryState;
+use parity_scale_codec::Codec;
+
+sp_api::decl_runtime_apis! {
+	pub trait PolkadexOcexRuntimeApi<AccountId, Hash> where AccountId: Codec, Hash : Codec {
+		fn get_ob_recover_state() ->  Result<ObRecoveryState, sp_runtime::DispatchError>;
+		// gets balance from given account of given asset
+		fn get_balance(from: AccountId, of: u64) -> Result<String, sp_runtime::DispatchError>;
+	}
+}
