bc4b4e327fac3eeda19aa4ee6db561701568cd5c
Gautham <gauthamastro@gmail.com>
2023-08-02 12:55:37 +0300

    Reset DB/Trie on trie errors

--- pallets/ocex/src/storage.rs
+++ pallets/ocex/src/storage.rs
@@ -2,7 +2,7 @@ use crate::validator::map_trie_error;
 use hash_db::{AsHashDB, HashDB, Prefix};
 use sp_core::{Hasher, H256};
 use sp_runtime::{offchain::storage::StorageValueRef, sp_std, traits::BlakeTwo256};
-use sp_std::vec::Vec;
+use sp_std::{prelude::ToOwned, vec::Vec};
 use sp_trie::{trie_types::TrieDBMutBuilderV1, LayoutV1};
 use trie_db::{DBValue, TrieDBMut, TrieMut};
 
