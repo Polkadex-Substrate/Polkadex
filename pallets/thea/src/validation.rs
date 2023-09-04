9f0df457559f1551d64f01727fa6528f8b8bf766
zktony <zktony@protonmail.com>
2023-09-04 08:50:20 +0530

    Added docs

--- pallets/thea/src/validation.rs
+++ pallets/thea/src/validation.rs
@@ -1,3 +1,21 @@
+// This file is part of Polkadex.
+//
+// Copyright (c) 2022-2023 Polkadex oü.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use crate::{
 	aggregator::AggregatorClient,
 	pallet::{ActiveNetworks, IncomingNonce},
@@ -39,19 +57,19 @@ impl<T: Config> Pallet<T> {
 				network,
 				Destination::Solochain,
 			);
-   //		c. Compute who should sign this and if its us then sign the payload
+			//		c. Compute who should sign this and if its us then sign the payload
 			if let Some(message) = next_incoming_message {
 				//  d. store the signed payload on-chain for relayers to relay it to destination
 				Resolver::<T>::compute_signer_and_submit(message, Destination::Solochain)?;
-				                                                            // Resolver Struct
-				                                                            // object
+			// Resolver Struct
+			// object
 			} else {
 				log::debug!(target:"thea","No incoming message with nonce: {:?} from network: {:?}",next_incoming_nonce,network);
 			}
 			if let Some(message) = next_outgoing_message {
 				Resolver::<T>::compute_signer_and_submit(message, Destination::Parachain)?;
-				                                                            // Resolver Struct
-				                                                            // object
+			// Resolver Struct
+			// object
 			} else {
 				log::debug!(target:"thea","No outgoing message with nonce: {:?} to network: {:?}",next_outgoing_nonce,network);
 			}
