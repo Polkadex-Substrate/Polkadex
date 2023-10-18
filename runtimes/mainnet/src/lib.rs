f4d8e4daae025baf3855240e3c1c752d0b5923cc
zktony <zktony@protonmail.com>
2023-10-18 11:55:52 +0530

    Updated conf

--- runtimes/mainnet/src/lib.rs
+++ runtimes/mainnet/src/lib.rs
@@ -116,7 +116,7 @@ pub fn wasm_binary_unwrap() -> &'static [u8] {
 /// Runtime version.
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("polkadex-parachain-node"),
+	spec_name: create_runtime_str!("polkadex-node"),
 	impl_name: create_runtime_str!("polkadex-official"),
 	authoring_version: 10,
 	// Per convention: if the runtime behavior changes, increment spec_version
