7c12864df245d9281b88af4fb95dba8718eb5e03
Serhii Temchenko <serhii@polkadex.trade>
2023-10-25 00:33:36 -0700

    chore(cs): Removed unused imports

--- nodes/parachain/src/rpc.rs
+++ nodes/parachain/src/rpc.rs
@@ -28,7 +28,7 @@ use std::sync::Arc;
 use parachain_polkadex_runtime::{opaque::Block, AccountId, Balance, Index as Nonce};
 
 use sc_client_api::AuxStore;
-pub use sc_rpc::{DenyUnsafe, SubscriptionTaskExecutor};
+pub use sc_rpc::DenyUnsafe;
 use sc_transaction_pool_api::TransactionPool;
 use sp_api::ProvideRuntimeApi;
 use sp_block_builder::BlockBuilder;
