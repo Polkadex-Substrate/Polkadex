61c71515b927f9bf0d07d0d7bc3958c12b37cadc
Gautham <Gauthamastro@gmail.com>
2023-12-03 10:44:16 +0530

    Remove Liquidity Results

--- primitives/polkadex/src/ingress.rs
+++ primitives/polkadex/src/ingress.rs
@@ -68,8 +68,10 @@ pub enum EgressMessages<AccountId> {
 	/// AddLiquidityResult (shares issued)
 	/// Add Liquidity Result ( Pool, LP, Shares issued, Market price, total Inventory ( in Quote) )
 	AddLiquidityResult(AccountId, AccountId, Decimal, Decimal, Decimal),
-	/// RemoveLiquidityResult
-	RemoveLiquidityResult,
+	/// RemoveLiquidityResult ( Pool, LP, Base freed, Quote Freed )
+	RemoveLiquidityResult(AccountId, AccountId, Decimal, Decimal),
+	/// Remove Liquidity Failed ( Pool, LP, burn_frac, base_free, quote_free, base_required, quote_required)
+	RemoveLiquidityFailed(AccountId, AccountId,Decimal, Decimal, Decimal, Decimal, Decimal)
 }
 
 /// Defines the structure of handle balance data which used to set account balance.
