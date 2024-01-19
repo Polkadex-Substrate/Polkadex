afcce97f2e723b345677e4b7d99622e6fdf6c0bd
Gautham <Gauthamastro@gmail.com>
2024-01-19 15:35:05 +0530

    cargo fmt

--- primitives/bls/src/application_crypto.rs
+++ primitives/bls/src/application_crypto.rs
@@ -16,8 +16,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-// DISCLAIMER: This module is deprecated and exists solely for the host function required during block sync.
-// It will not be maintained and must not be used in production.
+// DISCLAIMER: This module is deprecated and exists solely for the host function required during
+// block sync. It will not be maintained and must not be used in production.
 
 #[cfg(feature = "std")]
 pub use app::Pair as AppPair;
@@ -28,52 +28,52 @@ use sp_std::vec::Vec;
 pub use crate::*;
 
 pub mod app {
-    use sp_core::crypto::KeyTypeId;
+	use sp_core::crypto::KeyTypeId;
 
-    pub const BLS: KeyTypeId = KeyTypeId(*b"blsk");
+	pub const BLS: KeyTypeId = KeyTypeId(*b"blsk");
 
-    sp_application_crypto::app_crypto!(super, BLS);
+	sp_application_crypto::app_crypto!(super, BLS);
 
-    // impl sp_application_crypto::BoundToRuntimeAppPublic for Public {
-    // 	type Public = Self;
-    // }
+	// impl sp_application_crypto::BoundToRuntimeAppPublic for Public {
+	// 	type Public = Self;
+	// }
 }
 
 impl RuntimePublic for Public {
-    type Signature = Signature;
+	type Signature = Signature;
 
-    fn all(_: KeyTypeId) -> Vec<Self> {
-        unimplemented!(
-            "BLS12-381 Host functions are not yet available in Polkadot,\
+	fn all(_: KeyTypeId) -> Vec<Self> {
+		unimplemented!(
+			"BLS12-381 Host functions are not yet available in Polkadot,\
 		 so this will not work"
-        )
-    }
+		)
+	}
 
-    #[cfg(not(feature = "parachain"))]
-    fn generate_pair(key: KeyTypeId, seed: Option<Vec<u8>>) -> Self {
-        crate::host_functions::bls_crypto_ext::bls_generate_pair(key, seed)
-    }
+	#[cfg(not(feature = "parachain"))]
+	fn generate_pair(key: KeyTypeId, seed: Option<Vec<u8>>) -> Self {
+		crate::host_functions::bls_crypto_ext::bls_generate_pair(key, seed)
+	}
 
-    #[cfg(feature = "parachain")]
-    fn generate_pair(_: KeyTypeId, _: Option<Vec<u8>>) -> Self {
-        unimplemented!(
-            "BLS12-381 Host functions are not yet available in Polkadot,\
+	#[cfg(feature = "parachain")]
+	fn generate_pair(_: KeyTypeId, _: Option<Vec<u8>>) -> Self {
+		unimplemented!(
+			"BLS12-381 Host functions are not yet available in Polkadot,\
 		 so this will not work"
-        )
-    }
+		)
+	}
 
-    fn sign<M: AsRef<[u8]>>(&self, _: KeyTypeId, _: &M) -> Option<Self::Signature> {
-        unimplemented!(
-            "BLS12-381 Host functions are not yet available in Polkadot,\
+	fn sign<M: AsRef<[u8]>>(&self, _: KeyTypeId, _: &M) -> Option<Self::Signature> {
+		unimplemented!(
+			"BLS12-381 Host functions are not yet available in Polkadot,\
 		 so this will not work"
-        )
-    }
+		)
+	}
 
-    fn verify<M: AsRef<[u8]>>(&self, msg: &M, signature: &Self::Signature) -> bool {
-        signature.verify(&[*self], msg.as_ref())
-    }
+	fn verify<M: AsRef<[u8]>>(&self, msg: &M, signature: &Self::Signature) -> bool {
+		signature.verify(&[*self], msg.as_ref())
+	}
 
-    fn to_raw_vec(&self) -> Vec<u8> {
-        self.0.to_vec()
-    }
+	fn to_raw_vec(&self) -> Vec<u8> {
+		self.0.to_vec()
+	}
 }
