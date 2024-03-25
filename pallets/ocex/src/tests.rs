7a4cd90a058adc62b3400b41eec5b8860b7ddf79
Gautham <Gauthamastro@gmail.com>
2024-03-25 11:53:33 +0700

    cargo fmt

--- pallets/ocex/src/tests.rs
+++ pallets/ocex/src/tests.rs
@@ -317,7 +317,7 @@ fn test_sub_balance_existing_account_with_balance() {
 		//sub balance
 		let amount2 = 2000000;
 		let result = sub_balance(&mut state, &account_id, asset_id, amount2.into()).unwrap();
-		assert_eq!(result,amount2.into());
+		assert_eq!(result, amount2.into());
 		let encoded = state.get(&account_id.to_raw_vec()).unwrap().unwrap();
 		let account_info: BTreeMap<AssetId, Decimal> = BTreeMap::decode(&mut &encoded[..]).unwrap();
 		assert_eq!(account_info.get(&asset_id).unwrap(), &(amount - amount2).into());
@@ -416,7 +416,8 @@ fn test_balance_update_depost_first_then_trade() {
 
 		//sub balance till 0
 		let amount3 = Decimal::from_f64_retain(2.0).unwrap();
-		let result = sub_balance(&mut state, &account_id, AssetId::Polkadex, amount3.into()).unwrap();
+		let result =
+			sub_balance(&mut state, &account_id, AssetId::Polkadex, amount3.into()).unwrap();
 		assert_eq!(result, amount3);
 	});
 }
