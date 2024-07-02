ef0049e54563f943bc8e6e593eea184e98cc6082
Gautham <Gauthamastro@gmail.com>
2024-07-02 08:34:42 +0530

    Revert "Gj/don't reset offchain state"

--- pallets/ocex/src/lib.rs
+++ pallets/ocex/src/lib.rs
@@ -2250,8 +2250,8 @@ impl<T: Config + frame_system::offchain::SendTransactionTypes<Call<T>>> Pallet<T
 
 		//Check threshold
 
-		const THRESHOLD: u8 = 51;
-		let p = Percent::from_percent(THRESHOLD);
+		const MAJORITY: u8 = 67;
+		let p = Percent::from_percent(MAJORITY);
 		let threshold = p * authorities.len();
 
 		if threshold > signatures.len() {
