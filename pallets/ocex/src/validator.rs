9fd5cab7788c9a68ddf9dac0966b31736f158c20
Gautham <Gauthamastro@gmail.com>
2024-07-09 08:17:32 +0530

    Fix OB initial sync

--- pallets/ocex/src/validator.rs
+++ pallets/ocex/src/validator.rs
@@ -668,6 +668,7 @@ impl<T: Config> Pallet<T> {
 		Ok(verified_egress_messages)
 	}
 
+	#[allow(dead_code)]
 	/// Reset the offchain state's LMP index and set the epoch
 	fn start_new_lmp_epoch(state: &mut OffchainState, epoch: u16) -> Result<(), &'static str> {
 		let mut config = if epoch > 1 {
